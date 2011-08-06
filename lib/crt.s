6d75c406da7469a9fd676f3f525975f7a86f9419
Rob Pilling <robpilling@gmail.com>
2011-08-06 16:26:38 +0100

    Added lib

---
+++ lib/crt.s
@@ -0,0 +1,29 @@
+section .text
+	global _start
+	global write
+	global exit
+	extern main
+
+_start:
+	call main
+	mov rax, 0
+	push rax
+	call exit
+
+exit:
+	mov rax, 60
+	pop rdi
+	pop rdi
+	syscall
+	hlt
+
+write:
+	push rbp
+	mov rbp, rsp
+	mov rax, 1
+	mov rdi, [rbp + 16]
+	mov rsi, [rbp + 24]
+	mov rdx, [rbp + 32]
+	syscall
+	leave
+	ret
