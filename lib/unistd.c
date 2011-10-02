624c14f5baace983c884fd90aaaf293bc5b6cc0b
Rob Pilling <robpilling@gmail.com>
2011-10-02 15:05:56 +0100

    Arg order and ptr addition fixes

--- lib/unistd.c
+++ lib/unistd.c
@@ -3,10 +3,10 @@
 
 int read(int fd, void *p, int size)
 {
-	return __syscall(SYS_read, size, p, fd);
+	return __syscall(SYS_read, fd, p, size);
 }
 
 int write(int fd, void *p, int size)
 {
-	return __syscall(SYS_write, size, p, fd);
+	return __syscall(SYS_write, fd, p, size);
 }
