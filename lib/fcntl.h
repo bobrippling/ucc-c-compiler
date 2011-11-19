cb01a9d8ea390cc617017d70045dcc0468c16b6b
Rob Pilling <robpilling@gmail.com>
2011-11-19 13:07:23 +0000

    Fixed open(), removed comments from macros

--- lib/fcntl.h
+++ lib/fcntl.h
@@ -1,15 +1,15 @@
 #define O_RDONLY	     00
 #define O_WRONLY	     01
 #define O_RDWR		     02
-#define O_CREAT		   0100	/* not fcntl */
-#define O_EXCL		   0200	/* not fcntl */
-#define O_NOCTTY	   0400	/* not fcntl */
-#define O_TRUNC		  01000	/* not fcntl */
+#define O_CREAT		   0100
+#define O_EXCL		   0200
+#define O_NOCTTY	   0400
+#define O_TRUNC		  01000
 #define O_APPEND	  02000
 
-#define O_DIRECTORY	0200000	/* Must be a directory.	 */
-#define O_NOFOLLOW	0400000	/* Do not follow links.	 */
-#define O_CLOEXEC   02000000 /* Set close_on_exec.  */
+#define O_DIRECTORY	0200000
+#define O_NOFOLLOW	0400000
+#define O_CLOEXEC   02000000
 
 
-int open(const char *fname, int mode, int perm);
+int open(const char *fname, int mode, ...); /* third arg iff O_CREAT */
