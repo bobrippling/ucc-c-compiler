690226f16b38897140401d75f57385332fb2e5f5
Rob Pilling <rob pilling at gmail>
2011-12-04 23:47:28 +0000

    Added bugs/

---
+++ bugs/ucc_symtab_broke.c
@@ -0,0 +1,18 @@
+main()
+{
+    int level1;
+
+    {
+        int level2_v1_1;
+        int level2_v1_2;
+    }
+
+    {
+        int level2_v2_1;
+        int level2_v2_2;
+        {
+            int level3_1;
+            int level3_2;
+        }
+    }
+}
