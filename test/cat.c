85c7a2f7e08821dabf2d3202a15f11b2bbd806a0
Rob Pilling <robpilling@gmail.com>
2011-12-05 23:02:21 +0000

    Updated cat.c

--- test/cat.c
+++ test/cat.c
@@ -1,28 +1,32 @@
-#include "lib/fcntl.h"
-#include "unistd.h"
-#include "stdio.h"
+#include "lib/stdio.h"
+#include "lib/string.h"
+#include "lib/errno.h"
 
-cat(int fd)
+#include "lib/fcntl.h"
+#include "lib/unistd.h"
+
+
+void cat(int fd)
 {
 	char buf[8];
 	int n;
 
-	while(n = read(fd, buf, 8) > 0)
+	while((n = read(fd, buf, 8)) > 0)
 		write(1, buf, n);
 }
 
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int fd, i;
-
 	if(argc == 1){
-		printf("%s: reading from stdin...\n", *argv);
+		fprintf(stderr, "%s: reading from stdin...\n", *argv);
 		cat(0);
 	}else{
+		int i;
+
 		for(i = 1; i < argc; i++){
-			fd = open(argv[i], O_RDONLY, 0644);
+			int fd = open(argv[i], O_RDONLY);
 			if(fd == -1){
-				fprintf(stderr, "open \"%s\": [err]\n", argv[i]);
+				fprintf(stderr, "open \"%s\": %s\n", argv[i], strerror(errno));
 			}else{
 				cat(fd);
 				close(fd);
