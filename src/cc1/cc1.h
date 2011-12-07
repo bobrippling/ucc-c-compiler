3eb4bfbc2df7ab9fa557647b7940d394766d3aa7
Rob Pilling <rob pilling at gmail>
2011-12-07 23:42:54 +0000

    Structures for -W...

--- src/cc1/cc1.h
+++ src/cc1/cc1.h
@@ -9,6 +9,21 @@ enum section_type
 	NUM_SECTIONS
 };
 
+enum warning
+{
+	WARN_NONE             = 0,
+	WARN_ARG_MISMATCH     = 1 << 1,
+	WARN_ARRAY_COMMA      = 1 << 2,
+	WARN_ASSIGN_MISMATCH  = 1 << 3,
+	WARN_RETURN_TYPE      = 1 << 4,
+	WARN_SIGN_COMPARE     = 1 << 5,
+	WARN_EXTERN_ASSUME    = 1 << 6,
+	WARN_IMPLICIT_FUNC    = 1 << 7,
+	WARN_IMPLICIT_INT     = 1 << 8,
+};
+
+void cc1_warn_at(struct where *, enum warning, const char *, ...);
+
 extern FILE *cc_out[NUM_SECTIONS];
 extern FILE *cc1_out;
 
