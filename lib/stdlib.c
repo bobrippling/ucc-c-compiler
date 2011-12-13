7a76a63991a93a26b5b962d3e40cfbe9268485d3
Rob Pilling <robpilling@gmail.com>
2011-12-13 21:57:30 +0000

    Added atoi()

--- lib/stdlib.c
+++ lib/stdlib.c
@@ -5,3 +5,16 @@ void exit(int code)
 {
 	__syscall(SYS_exit, code);
 }
+
+int atoi(char *s)
+{
+	int i = 0;
+
+	while(*s)
+		if('0' <= *s && *s <= '9')
+			i = 10 * i + *s++ - '0';
+		else
+			break;
+
+	return i;
+}
