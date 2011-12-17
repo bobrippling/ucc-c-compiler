63708c4621d9154f3d74ec432dc7343c64720a2f
Rob Pilling <robpilling@gmail.com>
2011-12-17 13:00:35 +0000

    Temporary waitpid() and friends

--- lib/unistd.c
+++ lib/unistd.c
@@ -64,6 +64,11 @@ pid_t waitpid(int pid, int *status, int options)
 	return __syscall(SYS_wait4, pid, status, options, NULL);
 }
 
+int WEXITSTATUS(int stat)
+{
+	return (stat & 0xff00) >> 8;
+}
+
 /*pid_t wait(int *status)
 {
 	return waitpid(-1, status, 0);
--- lib/unistd.h
+++ lib/unistd.h
@@ -8,8 +8,32 @@ void *sbrk(int inc);
 // TODO: typedef
 #define pid_t int
 
+// TODO: move to sys/wait, and bring fcntl along too
 pid_t fork(void);
 //pid_t wait(int *status); FIXME
 pid_t waitpid(int pid, int *status, int options);
 
 #define NULL (void *)0
+
+int WEXITSTATUS(int);
+
+#ifdef DONT_HAVE_MACRO_FUNC_SUPPORT_YET
+/* If WIFEXITED(STATUS), the low-order 8 bits of the status.  */
+#define	__WEXITSTATUS(status)	(((status) & 0xff00) >> 8)
+
+/* If WIFSIGNALED(STATUS), the terminating signal.  */
+#define	__WTERMSIG(status)	((status) & 0x7f)
+
+/* If WIFSTOPPED(STATUS), the signal that stopped the child.  */
+#define	__WSTOPSIG(status)	__WEXITSTATUS(status)
+
+/* Nonzero if STATUS indicates normal termination.  */
+#define	__WIFEXITED(status)	(__WTERMSIG(status) == 0)
+
+/* Nonzero if STATUS indicates termination by a signal.  */
+#define __WIFSIGNALED(status) \
+  (((signed char) (((status) & 0x7f) + 1) >> 1) > 0)
+
+/* Nonzero if STATUS indicates the child is stopped.  */
+#define	__WIFSTOPPED(status)	(((status) & 0xff) == 0x7f)
+#endif
