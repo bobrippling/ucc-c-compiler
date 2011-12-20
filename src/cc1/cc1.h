88c2b2343ede3b840ac57d5e21736ae0b2177fcc
Rob Pilling <rob@jeffraw>
2011-12-20 01:20:43 +0000

    Factorised decl comparisons

--- src/cc1/cc1.h
+++ src/cc1/cc1.h
@@ -15,15 +15,17 @@ enum warning
 	WARN_ARG_MISMATCH     = 1 << 1,
 	WARN_ARRAY_COMMA      = 1 << 2,
 	WARN_ASSIGN_MISMATCH  = 1 << 3,
-	WARN_RETURN_TYPE      = 1 << 4,
-	WARN_SIGN_COMPARE     = 1 << 5,
-	WARN_EXTERN_ASSUME    = 1 << 6,
-	WARN_IMPLICIT_FUNC    = 1 << 7,
-	WARN_IMPLICIT_INT     = 1 << 8,
-	WARN_VOID_ARITH       = 1 << 9,
+	WARN_COMPARE_MISMATCH = 1 << 4,
+	WARN_RETURN_TYPE      = 1 << 5,
+	WARN_SIGN_COMPARE     = 1 << 6,
+	WARN_EXTERN_ASSUME    = 1 << 7,
+	WARN_IMPLICIT_FUNC    = 1 << 8,
+	WARN_IMPLICIT_INT     = 1 << 9,
+	WARN_VOID_ARITH       = 1 << 10,
 };
 
-void cc1_warn_at(struct where *where, int die, enum warning w, const char *fmt, ...);
+void cc1_warn_atv(struct where *where, int die, enum warning w, const char *fmt, va_list l);
+void cc1_warn_at( struct where *where, int die, enum warning w, const char *fmt, ...);
 
 extern FILE *cc_out[NUM_SECTIONS];
 extern FILE *cc1_out;
