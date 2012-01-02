eccb6c877b522d603b35ebae7d159f7e87221329
Rob Pilling <robpilling@gmail.com>
2012-01-02 01:58:42 +0000

    Split into parse.c and macro.c

--- src/cpp2/macro.h
+++ src/cpp2/macro.h
@@ -1,11 +1,44 @@
 #ifndef MACRO_H
 #define MACRO_H
 
-void macro_add_dir(const char *);
+typedef struct
+{
+	enum tok
+	{
+		TOKEN_WORD,
+		TOKEN_OPEN_PAREN,
+		TOKEN_CLOSE_PAREN,
+		TOKEN_COMMA,
+		TOKEN_OTHER
+	} tok;
+	char *w;
+	int had_whitespace;
+} token;
+
+
+typedef struct
+{
+	char *nam, *val;
+	char **args;
+} macro;
+
+void macro_add_dir(char *d);
 void macro_add(const char *nam, const char *val);
+macro *macro_find(const char *sp);
+void macro_remove(const char *nam);
 
 void filter_macro(char **line);
 void handle_macro(char *line);
 void macro_finish(void);
+
+
+#define TODO() do{fprintf(stderr, "%s: TODO\n", __func__); exit(1);}while(0)
+extern int debug;
+enum
+{
+	DEBUG_NORM = 0,
+	DEBUG_VERB = 1,
+};
+#define DEBUG(level, ...) if(level < debug) fprintf(stderr, ">>> " __VA_ARGS__)
 
 #endif
---
+++ src/cpp2/parse.h
@@ -0,0 +1,6 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+int should_noop(void);
+
+#endif
