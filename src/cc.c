1be04f70022ad3388dcc2d75642cdaf9fa5854c9
Rob Pilling <rob@jeffraw>
2012-01-02 01:59:08 +0000

    Switched to cpp2

--- src/cc.c
+++ src/cc.c
@@ -13,6 +13,7 @@
 #include "util/alloc.h"
 
 #define LIB_PATH "/../lib/"
+#define CPP "cpp2"
 
 enum mode
 {
@@ -205,7 +206,7 @@ unknown_file:
 	}
 
 	SHORTEN_OUTPUT(MODE_PREPROCESS, f_e);
-	RUN(1, "cpp/cpp %s -I'%s" LIB_PATH "' %s -o %s %s", debug ? "-v" : "", where, args[MODE_PREPROCESS], f_e, input);
+	RUN(1, CPP "/cpp %s -I'%s" LIB_PATH "' %s -o %s %s", debug ? "-v" : "", where, args[MODE_PREPROCESS], f_e, input);
 	if(mode == MODE_PREPROCESS)
 		return 0;
 
