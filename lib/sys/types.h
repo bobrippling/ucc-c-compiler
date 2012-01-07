b75c12ca7d90ddde69fb4794902341a90be3ac93
Rob Pilling <rob pilling at gmail>
2012-01-07 14:21:18 +0000

    Updated lib to use va_*

--- lib/sys/types.h
+++ lib/sys/types.h
@@ -13,7 +13,7 @@ typedef unsigned char  uint8_t;
 //typedef signed   short  int16_t;
 //typedef unsigned short uint16_t;
 
-typedef signed   int    int32_t;
+typedef signed   int    int32_t; // FIXME: sizeof(int) is currently 64, not 32
 typedef unsigned int   uint32_t;
 
 #ifdef __x86_64__
@@ -23,6 +23,6 @@ typedef unsigned int   uint32_t;
 
 
 typedef unsigned int  size_t;
-typedef          int ssize_t;
+typedef   signed int ssize_t;
 
 #endif
