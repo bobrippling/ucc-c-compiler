b05c819a1363dc19eac492e8bb5eab04dde9ddce
Rob Pilling <rob pilling at gmail>
2012-01-07 15:28:25 +0000

    Added char i/o and fget[cs] functions

--- lib/stdio.h
+++ lib/stdio.h
@@ -6,13 +6,34 @@ typedef int size_t;
 
 extern FILE *stdin, *stdout, *stderr;
 
+#define EOF -1
+
 /* va_list */
 #include "stdarg.h"
 
-int puts( const char *);
-int printf(const char *, ...);
+
+/* writing */
 int fprintf(FILE *, const char *, ...);
+int  printf(const char *, ...);
 
 int dprintf(int, const char *, ...);
+
+int fputs(const char *, FILE *);
+int  puts(const char *);
+
+int fputc(int, FILE *);
+#define putc fputc
+int  putchar(int);
+
+/* writing, variadic */
+int vfprintf(FILE *, char *, va_list);
+
+
+/* reading */
+int   getchar(void);
+#define getc fgetc
+int   fgetc(FILE *);
+/*char *gets( char *);*/
+char *fgets(char *, int, FILE *);
 
 #endif
