717f645d2b1592400fed72b543fefc238d84d158
Rob Pilling <robpilling@gmail.com>
2012-01-17 22:51:53 +0000

    Updated struct code

--- bugs/struct.c
+++ bugs/struct.c
@@ -1,79 +1,116 @@
-#define STRUCT_PTR
+#define BASIC
+//#define SIMPLE
+//#define NESTED
+//#define NAMING
+//#define NESTED_DIFFERENT
 
-#ifdef PRE_DECL_STRUCT
-struct timmy
-{
-	int i;
-	void *p;
-};
-
+#ifdef BASIC
 main()
 {
-	struct timmy tim; // FIXME
-	1;
-}
-#endif
-
-#ifdef STRUCT_PTR
-main()
-{
-	int array_hack[64];
-	struct int_struct_ptr
+	struct
 	{
-		int i;
-		struct ptr_struct
-		{
-			int *p;
-		} sub;
-		void *p;
-	} y, *x;
+		int i, j;
+		int *p;
+	} *x, y;
 
-	//x = array_hack;
 	x = &y;
 
 	x->i = 5;
+	x->j = 2;
 
-	y.i = 2;
+	y.i = 3;
 
 	return x->i;
 }
 #endif
 
-#ifdef TEST_STRUCT_EXPR
+#ifdef SIMPLE
 main()
 {
 	struct
 	{
+		int a, b;
+		struct
+		{
+			int d, e;
+		} sub;
+		int c;
+	} *list, actual;
+
+	list = &actual;
+
+	list->a = 1;
+	list->b = 2;
+	list->c = 3;
+	list->sub->d = 4;
+	list->sub->e = 5;
+
+	return list->sub->d + list->c;
+}
+#endif
+
+#ifdef NESTED
+main()
+{
+	struct int_struct_ptr
+	{
 		int i;
-	} *tim;
-	int *p;
+		struct ptr_struct
+		{
+			void *p;
+			int i;
+		} sub;
+		void *p;
+	} x;
 
-	p = &tim->i;
+	x.sub.i = 5;
+
+	return x.sub.i;
 }
 #endif
 
-#ifdef TEST_ALLOC
-main()
+#ifdef NAMING
+struct timmy
 {
-	struct { int i; int *p; } *st;
+	int i;
+	void *p;
+};
+struct timmy tim; // FIXME
 
-	st = malloc(sizeof *st);
-
-	st->p = &st->i;
-	*st->p = 5;
-
-	return st->i;
-}
+struct
+{
+	struct
+	{
+		int i;
+	}; // TODO: empty decl or whatever gcc says
+	int j;
+} tim;
 #endif
 
-
-#ifdef TEST_DOT
+#ifdef NESTED_DIFFERENT
 main()
 {
-	int q;
-	x.sub.p = &x.i;
-	*x.sub.p = 5;
+	struct C
+	{
+		int a;
+	};
 
-	return x.i;
+	struct B
+	{
+		int a;
+		struct C c;
+	} st_b ;//= { 0, 0 };
+
+	struct A
+	{
+		int a;
+		struct B *b;
+	} st_a;
+
+	st_b.c.a = 3;
+
+	st_a.b = &st_b;
+
+	return st_a.b->c.a;
 }
 #endif
