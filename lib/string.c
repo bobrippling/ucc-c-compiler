bb2820a0ca29bce951e43388d5529589e31022bc
Rob Pilling <rob@jeffraw>
2012-02-03 22:21:20 +0000

    Added possibly-working string functions

--- lib/string.c
+++ lib/string.c
@@ -1,4 +1,6 @@
 #include "string.h"
+#include "sys/types.h"
+#include "limits.h"
 
 static const char *_errs[] = {
 #include "string_strerrs.h"
@@ -17,4 +19,32 @@ const char *strerror(int eno)
 {
 	//extern const char *_errs[];
 	return _errs[eno - 1];
+}
+
+int strncmp(char *a, char *b, size_t n)
+{
+	while(n > 0){
+		int x = *b - *a;
+		if(x)
+			return x;
+		n--;
+		a++;
+		b++;
+	}
+	return 0;
+}
+
+int strcmp(char *a, char *b)
+{
+	return strncmp(a, b, UINT_MAX);
+}
+
+char *strchr(char *s, char c)
+{
+	while(*s)
+		if(*s == c)
+			return s;
+		else
+			s++;
+	return NULL;
 }
