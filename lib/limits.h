0557d2647c3f02c555474177e24e90bf1a5f9304
Rob Pilling <robpilling@gmail.com>
2012-02-03 22:21:20 +0000

    Possibly working string fns + limits.h

---
+++ lib/limits.h
@@ -0,0 +1,39 @@
+#ifndef LIMITS_H
+#define LIMITS_H
+
+# define CHAR_BIT  8
+
+# define SCHAR_MIN  -128
+# define SCHAR_MAX   127
+
+# define UCHAR_MAX   255
+
+# define CHAR_MIN  SCHAR_MIN
+# define CHAR_MAX  SCHAR_MAX
+
+# define SHRT_MIN  -32768
+# define SHRT_MAX   32767
+
+# define USHRT_MAX  65535
+
+# define INT_MIN  (-INT_MAX - 1)
+# define INT_MAX    2147483647
+
+# define UINT_MAX  4294967295
+
+/*
+# if __WORDSIZE == 64
+#  define LONG_MAX  9223372036854775807L
+# else
+#  define LONG_MAX  2147483647L
+# endif
+# define LONG_MIN  (-LONG_MAX - 1L)
+
+# if __WORDSIZE == 64
+#  define ULONG_MAX  18446744073709551615UL
+# else
+#  define ULONG_MAX  4294967295UL
+# endif
+*/
+
+#endif
--- lib/string.h
+++ lib/string.h
@@ -1,7 +1,14 @@
 #ifndef __STRING_H
 #define __STRING_H
 
+#include "macros.h"
+
 int strlen(const char *);
 const char *strerror(int eno);
+
+int strcmp( char *, char *);
+int strncmp(char *, char *, size_t);
+
+char *strchr(char *s, char c);
 
 #endif
