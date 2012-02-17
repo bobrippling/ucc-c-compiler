ed86b3aa12ae323d673ab946a423a8af429afed1
Rob Pilling <rob@jeffraw>
2012-02-17 23:14:40 +0000

    Updated struct test

--- test/st.c
+++ test/st.c
@@ -1,19 +1,35 @@
+#define NEST
+
 struct Tim
 {
 	int i;
 	int j;
+#ifdef NEST
+	struct
+	{
+		int w, x;
+	} st;
+#endif
 };
 
-set_j(struct Tim *tp)
+set(struct Tim *tp)
 {
+#ifdef NEST
+	tp->st->x = 3;
+#else
 	tp->j = 5;
+#endif
 }
 
 main()
 {
 	struct Tim t;
 
-	set_j(&t);
+	set(&t);
 
+#ifdef NEST
+	return t.st.x;
+#else
 	return t.j;
+#endif
 }
