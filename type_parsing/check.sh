9a10365cf2bcb01b96f4ebc8be69b9102b65bbb3
Rob Pilling <rob@jeffraw>
2012-02-19 16:46:50 +0000

    Updated type parsing checks - all working now

--- type_parsing/check.sh
+++ type_parsing/check.sh
@@ -5,14 +5,24 @@ trap "rm -f $log" EXIT
 
 d=$(dirname "$0")
 
-for f in $d/*.c
-do
-	$d/../src/cc $f > $log 2>&1
+ret=0
+
+check(){
+	$@ > $log 2>&1
 	if [ $? -ne 0 ]
 	then
 		printf '\e[1;31mbroke: %s:\e[m\n' "$f"
 		sed 's/^/	/' $log
-	fi >&2
+		ret=1
+	fi
+}
+
+for f in $d/*.c
+do
+	check $d/../src/cc $f
+	check ./a.out
 done
 
 rm -f a.out
+
+exit $ret
--- type_parsing/recursive.c
+++ type_parsing/recursive.c
@@ -1,5 +1,10 @@
 int x(int (*self)())
 {
+	static int x;
+	if(++x > 10){
+		printf("limit\n");
+		exit(0);
+	}
 	printf("hi\n");
 	self(self);
 }
