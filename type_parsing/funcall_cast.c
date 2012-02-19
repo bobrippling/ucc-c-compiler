16170ec0c109db14307e5127609c6780dbafe325
Rob Pilling <robpilling@gmail.com>
2012-02-19 12:16:05 +0000

    Added new funcall test

---
+++ type_parsing/funcall_cast.c
@@ -0,0 +1,20 @@
+#define IplImage void
+
+cvGetSize(), cvCreateImage();
+
+main()
+{
+	void *img;
+
+#ifdef __TINYC__
+	long long s = ((long long(*)(IplImage*))cvGetSize)(img);
+	IplImage* gray = ((IplImage*(*)(long long, int, int))cvCreateImage)(s, 8, 1);
+#else
+#  ifdef __UCC__
+	int s = ((int (*)(IplImage*))cvGetSize)(img);
+	IplImage *gray = ((IplImage *(*)(int, int, int))cvCreateImage)(s, 8, 1);
+#  else
+	IplImage *gray = cvCreateImage( cvGetSize(img), 8, 1 );
+#  endif
+#endif
+}
