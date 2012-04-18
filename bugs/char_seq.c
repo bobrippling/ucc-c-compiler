1c4826c7768615a8829bc16968d9b9768f93e53e
Rob Pilling <robpilling@gmail.com>
2012-04-19 00:09:45 +0100

    Updated bugs

--- bugs/char_seq.c
+++ bugs/char_seq.c
@@ -1,4 +1,11 @@
+#define assert(x) if(!(x)){printf("assertion %s failed\n", #x); return 1;}
 main()
 {
-	return '\xa' + '\11';
+	assert('\xa' == 10);
+	assert('\11' ==  9);
+	assert('\111' ==  73);
+	assert("\xb"[0] == 11);
+	0xff;
+
+	return 0;
 }
