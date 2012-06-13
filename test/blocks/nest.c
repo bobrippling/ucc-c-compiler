ff19ea9568788e5462f86fc018fb287f1a5d14a5
Rob Pilling <robpilling@gmail.com>
2012-06-13 22:31:40 +0100

    Nested block test

---
+++ test/blocks/nest.c
@@ -0,0 +1,17 @@
+main()
+{
+	return ^(int i){
+		int (^b)(int);
+
+		b = ^(int a){
+			return ^(int f){
+				return ({
+						int a = f;
+						a;
+					});
+			}(a);
+		};
+
+		return b(i);
+	}(5);
+}
