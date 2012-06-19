eaa462ef64e625b528901aa0d4111c33c364bd42
Rob Pilling <robpilling@gmail.com>
2012-06-19 22:37:24 +0100

    Added __attribute__((bitmask)) for enums

--- src/cc1/const.h
+++ src/cc1/const.h
@@ -4,6 +4,7 @@
 int const_fold(expr *e);
 int const_expr_is_const(expr *e);
 int const_expr_is_zero( expr *e);
+int const_expr_value(expr *e);
 
 #define POSSIBLE_OPT(e, s) \
 	cc1_warn_at(&e->where, 0, 1, WARN_OPT_POSSIBLE,  \
