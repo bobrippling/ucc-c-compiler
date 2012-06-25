ee888858254915335b37df26ccb89984bb1777c0
Rob Pilling <robpilling@gmail.com>
2012-06-26 00:19:03 +0100

    Modified test code to include more asserts

--- compound_assign.c
+++ compound_assign.c
@@ -13,10 +13,24 @@ int i, j;
 
 main()
 {
+#ifndef TEST
 	i = 0;
 	j = 10;
 
 	*f() += 2;
 
 	assert(i == 2 && j == 10);
+#else
+	int i = 5;
+
+	assert(i++ == 5);
+	assert(i == 6);
+	assert(++i == 7);
+
+	i++;
+
+	i /= 2;
+
+	assert(i == 4);
+#endif
 }
