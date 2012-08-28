347a154dd3881ff7c516551a651b7c0d1f7a077d
Rob Pilling <robpilling@gmail.com>
2012-08-28 23:58:17 +0100

    More ?: tests

--- expr_if.c
+++ expr_if.c
@@ -1,11 +1,14 @@
-#define NULL (void *)0
 main()
 {
 	struct A { int i; };
 	void *p = (__typeof(5)*)5;
 
-	printf("%d %d\n", ((struct { int i; } *)p)->i, (p ? NULL : (struct{int i;}*)3)->i);
+	/*
+	printf("%d %d\n",
+			((struct { int i; } *)p)->i,
+			(p ? NULL : (struct{int i;}*)3)->i);
+	*/
 
-	(1 ? (struct A *)0 :          NULL)->i;
-	(1 ? NULL          : (struct A *)0)->i;
+	x((1 ? (struct A *)0 :     (void *)0)->i);
+	x((1 ? (void *)0     : (struct A *)0)->i);
 }
--- expr_if2.c
+++ expr_if2.c
@@ -4,7 +4,7 @@ main()
 
 	//1 ? (struct A *)5 : 0;
 
-	0 ? (struct B *)0 : (int *)2; // should warn, and fold to (void *)2
+	x(0 ? (struct B *)0 : (int *)2); // should warn, and fold to (void *)2
 
 	//(0 ? (void *)0 : (struct defined *)0)->i;
 }
