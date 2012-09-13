9925250f0e37f53492dbb1976814c1d3db0d6b24
Rob Pilling <robpilling@gmail.com>
2012-09-13 21:28:19 +0100

    pack() packing correctly (alignment checks needed)

--- src/cc1/pack.c
+++ src/cc1/pack.c
@@ -4,20 +4,15 @@
 int pack_next(int offset, int sz, int align)
 {
 	static int word_size;
-	int next;
 
-	if(!word_size){
-		extern void *stderr;
-		extern int fprintf();
-		fprintf(stderr, "FIXME: %s\n", __func__);
-
+	if(!word_size)
 		word_size = platform_word_size();
-	}
 
-	/* TODO */
-	next = offset + sz + (offset + sz) % align;
+	offset += sz;
+	if(offset % align)
+		offset += align - offset % align;
 
 	/*printf("pack(%d, %d) -> %d\n", offset, sz, next);*/
 
-	return next;
+	return offset;
 }
