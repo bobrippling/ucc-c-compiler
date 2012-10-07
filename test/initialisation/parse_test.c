5630f1d74d6e36569c659724e908edb91f535a57
Rob Pilling <robpilling@gmail.com>
2012-10-07 10:43:35 +0100

    Added initialisation tests to target

--- test/initialisation/parse_test.c
+++ test/initialisation/parse_test.c
@@ -3,6 +3,6 @@ main()
 	int x[][2] = {
 		{ 0, 0 },
 		{ 1, 2 },
-		{ "hi", { .a = { 5, 2 } } }, // this bit should fail
+		//{ "hi", { .a = { 5, 2 } } }, // this bit should fail
 	}; // complete to x[3][2]
 }
