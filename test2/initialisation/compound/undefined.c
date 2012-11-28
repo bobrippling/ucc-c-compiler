d4203a53c3a560cd810148965d92304db4cfbb9c
Rob Pilling <robpilling@gmail.com>
2012-11-28 22:04:53 +0000

    Tidied up compound init tests

--- test2/initialisation/compound/undefined.c
+++ test2/initialisation/compound/undefined.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-typedef struct int_struct {
-  int x;
+typedef struct int_struct
+{
+	int x;
 } int_struct;
 
 #define MAX_INTS 10
 
 int main(void){
-  size_t i;
-  int_struct *ints[MAX_INTS];
+	size_t i;
+	int_struct *ints[MAX_INTS];
 
-  for (i = 0; i < MAX_INTS; i++) {
-    ints[i] = &(int_struct){i}; // needs struct-init parsing/fold/gen
-  }
+	for (i = 0; i < MAX_INTS; i++) {
+		ints[i] = &(int_struct){i}; // needs struct-init parsing/fold/gen
+	}
 
 	int sum = 0;
-  for (i = 0; i < MAX_INTS; i++) {
+	for (i = 0; i < MAX_INTS; i++) {
 		sum += ints[i]->x;
-    //printf("%d\n", ints[i]->x);
-  }
+		//printf("%d\n", ints[i]->x);
+	}
 
 	return sum;
 }
