b3da8f951aaa30a85c23c4fb52056f73c0a5d4d9
Rob Pilling <robpilling@gmail.com>
2013-01-26 23:22:31 +0000

    Silence tests

--- test2/all.sh
+++ test2/all.sh
@@ -5,9 +5,19 @@ if [ "$1" = -v ]
 then verbose=1
 fi
 
+ec=0
+
 find . -iname '*.c' | while read f
 do
 	[ $verbose -ne 0 ] && echo "$0: $f"
 
-	perl ./test.pl $@ "$f" || exit $?
+	perl ./test.pl $@ "$f" > /dev/null 2>&1 < /dev/null
+	r=$?
+	if [ $r -ne 0 ]
+	then
+		echo "fail: $f"
+		ec=1
+	fi
 done
+
+exit $ec
