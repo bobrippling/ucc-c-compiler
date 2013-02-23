0bb2e7bbfafea88782a93f7b5781b64040182642
Rob Pilling <robpilling@gmail.com>
2013-02-23 15:04:41 +0000

    Added more static-array-parameter tests

--- test2/arrays/as_params/static.c
+++ test2/arrays/as_params/static.c
@@ -3,11 +3,16 @@ int f(int x[static /*const*/ 10])
 	return *++x;
 }
 
+int g(int x[10]);
+
 main()
 {
 	int x[5];
 	x[1] = 2;
 	f(x);
+
+	g(x); // no warn
+
 	//int y[1];
 	//int *y;
 	//#define y (void *)0
