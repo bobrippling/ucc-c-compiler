ccdb63d5ccb2a552d207ac03020fe450c6828b6f
Rob Pilling <robpilling@gmail.com>
2013-02-27 23:12:24 +0000

    Updated constants test - different for decimal

--- test2/literals/constants.c
+++ test2/literals/constants.c
@@ -1,3 +1,5 @@
+// RUN: %ucc %s
+
 #define TY_ASSERT(n, ty)            \
 	_Static_assert(                   \
 			__builtin_types_compatible_p( \
@@ -13,7 +15,10 @@ TY_ASSERT(0xffffffff, unsigned);
 TY_ASSERT(0x100000000, long); // low limit
 TY_ASSERT(0x7fffffffffffffff, long); // high limit
 TY_ASSERT(0xffffffffffffffff, unsigned long);
-//TY_ASSERT(0x10000000000000000, unsigned long long);
+
+#ifdef HAVE_LONG_LONG
+TY_ASSERT(0x10000000000000000, unsigned long long)
+#endif
 
 // explicit suffixes
 TY_ASSERT(1L, long);
@@ -28,8 +33,24 @@ TY_ASSERT(0x7fffffffffffffffU, unsigned long); // no L-suffix, but promoted
 TY_ASSERT(0x7fffffffffffffffL,          long); // no U-suffix, signed
 TY_ASSERT(0xffffffffffffffffL, unsigned long); // no U-suffix, but promoted
 
-//TY_ASSERT(0x10000000000000000L, unsigned long); long long
-//TY_ASSERT(0xfffffffffffffffffffffffffffL, unsigned long long); // L-suffix, but promoted since it's too small
+#ifdef HAVE_LONG_LONG
+TY_ASSERT(0x10000000000000000L, unsigned long); long long
+TY_ASSERT(0xfffffffffffffffffffffffffffL, unsigned long long); // L-suffix, but promoted since it's too small
+#endif
+
+TY_ASSERT(2147483647,   signed);
+TY_ASSERT(4294967295,   long);
+TY_ASSERT(4294967295L,  long);
+TY_ASSERT(4294967295LU, unsigned long);
+TY_ASSERT(4294967296,   long);
+TY_ASSERT(1099511627775UL,     unsigned long);
+TY_ASSERT(9223372036854775807, long);
+TY_ASSERT(9223372036854775807U, unsigned long);
+TY_ASSERT(9223372036854775807L,          long);
+#ifdef HAVE_LONG_LONG
+TY_ASSERT(18446744073709551615, unsigned long long); // warn here about change to unsigned
+TY_ASSERT(18446744073709551615L, unsigned long long); // warn here
+#endif
 
 main()
 {
