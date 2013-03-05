11c6d901e15493ac23c64d081323172b02d6737c
Rob Pilling <robpilling@gmail.com>
2013-03-05 20:35:22 +0000

    Anonymous struct overwrite problem

--- anon.c
+++ anon.c
@@ -10,7 +10,8 @@ struct A
 	int k;
 };
 
-struct A a = { .j = 1, 2, 3 };
+struct A a = { .j = 1, .k = 2, .i = 0 };
+//struct A a = { .p9 = 2, .k = 3 };
 //struct A a = { .i = 1, 2, 3 };
 //struct A a = { .k = 1, 2, 3 };
 //struct A a = { { 1, 2 }, 3 };
