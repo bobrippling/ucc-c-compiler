b3a2ab6146c40f6bb08137fc0d9f629c19b61c80
Rob Pilling <robpilling@gmail.com>
2013-03-07 20:40:32 +0000

    Revert "The start for passing decl_inits through to code gen"
    This reverts commit 89ca9e4b12a70d1c8a0cac9689ae1255d5254545.
    This is a temporary revert so the following commits compile and can be tested.
    
    Conflicts:
    	src/cc1/decl_init.c

--- src/cc1/decl_init.c
+++ src/cc1/decl_init.c
@@ -457,7 +457,7 @@ static decl_init *decl_init_brace_up(decl_init *current, init_iter *iter, type_r
 	return decl_init_brace_up_scalar(current, iter, tfor);
 }
 
-static decl_init *decl_init_brace_up_start(decl_init *init, type_ref *tfor)
+static void DEBUG(decl_init *init, type_ref *tfor)
 {
 	decl_init *inits[2] = {
 		init, NULL
@@ -465,16 +465,25 @@ static decl_init *decl_init_brace_up_start(decl_init *init, type_ref *tfor)
 	init_iter it;
 	it.pos = inits;
 
-	return decl_init_brace_up(NULL, &it, tfor);
+	decl_init *braced = decl_init_brace_up(NULL, &it, tfor);
+
+	cc1_out = stdout;
+	printf("braced = %p\n", (void *)braced);
+	void print_decl_init(void *);
+	print_decl_init(braced);
+
+	exit(5);
 }
 
-void decl_init_brace_spel(decl *d, stmt *init_code)
+void decl_init_create_assignments_for_spel(decl *d, stmt *init_code)
 {
 	(void)init_code;
-	d->init = decl_init_brace_up_start(d->init, d->ref);
+	DEBUG(d->init, d->ref);
 }
 
-void decl_init_brace_base(decl *d, expr *base)
+void decl_init_create_assignments_for_base(decl *d, expr *base, stmt *init_code)
 {
-	d->init = decl_init_brace_up_start(d->init, d->ref);
+	(void)init_code;
+	(void)base;
+	DEBUG(d->init, d->ref);
 }
