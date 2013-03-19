194114e2ec0347ffc5ff072ae5b12821a64f91af
Rob Pilling <robpilling@gmail.com>
2013-03-19 23:49:56 +0000

    Compile fixes for INIT/ tests

--- INIT/range_etc.c
+++ INIT/range_etc.c
@@ -7,9 +7,13 @@ int widths[] = {
         [10 ... 99] = 2,
         [100] = 3
 };
-struct point p = { .y = yvalue, .x = xvalue };
-union foo f = { .d = 4 };
-int a[6] = {
+struct point {int x, y; } p = { .y = 1, .x = 2 };
+union foo { int d; } f = { .d = 4 };
+int b[6] = {
+#define v0 0
+#define v1 1
+#define v2 2
+#define v4 4
         [1] = v1,
         v2,
         [4] = v4
@@ -17,13 +21,13 @@ int a[6] = {
 int whitespace[256] = {
         [' '] = 1,
         ['\t'] = 1,
-        ['\h'] = 1,
+        //['\h'] = 1,
         ['\f'] = 1,
         ['\n'] = 1,
         ['\r'] = 1
 };
 struct point ptarray[10] = {
-        [2].y = yv2,
-        [2].x = xv2,
-        [0].x = xv0
+        [2].y = v2,
+        [2].x = v2,
+        [0].x = v0
 };
