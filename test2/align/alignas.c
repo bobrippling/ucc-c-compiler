4eb9e027d5e5aa03a79fa8867b5f5a96907ee3d6
Rob Pilling <robpilling@gmail.com>
2013-03-23 18:08:02 +0000

    _Alignof examples

---
+++ test2/align/alignas.c
@@ -0,0 +1,26 @@
+// RUN: %ucc -S -o- %s | grep 'subq.*24'
+// align of p is 8:
+// RUN: %ucc -S -o- %s | grep -B1 '^p:' | sed -n 1p | grep ' 8'
+
+char _Alignas(void *) p;
+
+main()
+{
+	int _Alignas(8) i; // 8
+	char _Alignas(int) c; // 4
+
+	_Alignas(sizeof(int)) char c2; // +4
+
+	_Alignas(void (*)()) pf; // 8
+
+	_Alignas(1) _Alignas(4) _Alignas(16) int j; // 16
+
+#define ALIGN(var, n) \
+	_Static_assert(_Alignof(var) == n, #var " alignment not " #n)
+
+	ALIGN(i, 8);
+	ALIGN(c, 4);
+	ALIGN(c2, 4);
+	ALIGN(pf, 8);
+	ALIGN(j, 16);
+}
---
+++ test2/align/alignas_struct.c
@@ -0,0 +1,15 @@
+struct align
+{
+	char pre;
+	char _Alignas(16) bet;
+	char post;
+} glob = { 1, 2, 3 };
+
+struct align al;
+
+stru()
+{
+	al.pre = 1;
+	al.bet = 2;
+	al.post = 3;
+}
