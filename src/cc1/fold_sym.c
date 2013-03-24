05af15fe9df7e7d83f449685d935b177d802f5d7
Rob Pilling <robpilling@gmail.com>
2013-03-24 17:00:37 +0000

    Correct size allocation when align > size

--- src/cc1/fold_sym.c
+++ src/cc1/fold_sym.c
@@ -95,7 +95,12 @@ int symtab_fold(symtable *tab, int current)
 						int siz = decl_size(s->decl);
 						int align = decl_align(s->decl);
 
-						/* packing takes care of everything */
+						/* align greater than size - we increase
+						 * size so it can be aligned to `align'
+						 */
+						if(align > siz)
+							siz = pack_to_align(siz, align);
+
 						pack_next(&current, NULL, siz, align);
 
 						s->offset = current;
