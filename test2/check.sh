21851afb9d8892c7193c96efd68bdeb02b52a615
Rob Pilling <robpilling@gmail.com>
2013-04-08 21:29:50 +0100

    Allow %check -e for error enforcing

--- test2/check.sh
+++ test2/check.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+error=0
+if [ "$1" = -e ] && [ $# -eq 2 ]
+then
+	shift
+	error=1
+fi
+
 if [ $# -ne 1 ]
 then
-	echo "Usage: $0 src" >&2
+	echo "Usage: $0 [-e] src" >&2
+	echo "-e: expect ucc to error" >&2
 	exit 1
 fi
 
@@ -14,10 +22,18 @@ trap "rm -f $e" EXIT
 $ucc -o/dev/null -c "$1" 2>$e
 r=$?
 if [ $r -ne 0 ]
+then r=1
+fi
+
+if [ $r -ne $error ]
 then
-	echo "make '$1':"
+	s=
+	if [ $error -eq 0 ]
+	then s="no "
+	fi
+	echo "${s}error expected"
 	cat $e
-	exit $r
+	exit 1
 fi >&2
 ./check.pl < $e "$1"
 exit $?
