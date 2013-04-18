62c360d7e09b0014b3c9da8d6a41eab89954586b
Rob Pilling <robpilling@gmail.com>
2013-04-18 23:32:30 +0100

    No preprocessing of vari.c for now + test case

--- vari.c
+++ vari.c
@@ -1,4 +1,20 @@
+//#include <stdarg.h>
+
+typedef __builtin_va_list va_list;
+
 f(int a, ...)
 {
+	va_list l;
 
+	__builtin_va_start(l, 5);
+	//vf(a, l);
+
+	return a + __builtin_va_arg(l, int);
+
+	//va_end(l);
+}
+
+main()
+{
+	return f(1, 2);
 }
