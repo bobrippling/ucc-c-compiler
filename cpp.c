ddfb7d93fc4f9b157f15ec6ebda9c87aef047655
Rob Pilling <robpilling@gmail.com>
2013-05-12 23:23:58 +0100

    More cpp cases

--- cpp.c
+++ cpp.c
@@ -1,3 +1,14 @@
+//#define F(a, b, c) a + b - c
+//#define A F
+//A()
+
+//#define J(a, b, ...) __VA_ARGS__ ## a ## b
+//J(1, 2, 3)
+
+//#define G(g) g * 5
+//#define F(x, y) x + G(y) //x + #y + y ## x
+//F(2, G(3))
+
 #define A(x) x + 1
 #define B(y) y + 2
 
