19f52971294f29d770a0c9a7283db87e4eadb2ad
Rob Pilling <robpilling@gmail.com>
2013-05-25 00:09:55 +0100

    decl init -Xstyle

--- src/cc1/gen_style.c
+++ src/cc1/gen_style.c
@@ -2,6 +2,8 @@
 #include <stdarg.h>
 
 #include "../util/util.h"
+#include "../util/dynarray.h"
+
 #include "data_structs.h"
 #include "expr.h"
 #include "stmt.h"
@@ -9,10 +11,42 @@
 #include "sym.h"
 #include "gen_style.h"
 #include "gen_asm.h" /* FIXME: gen_stmt/_expr should be in gen.h */
+#include "decl_init.h"
 
 void gen_style_dinit(decl_init *di)
 {
-	/* TODO */
+	switch(di->type){
+		case decl_init_scalar:
+			gen_expr(di->bits.expr);
+			break;
+
+		case decl_init_copy:
+			ICE("copy in gen");
+			break;
+
+		case decl_init_brace:
+		{
+			decl_init *s;
+			int i;
+
+			stylef("{");
+
+			for(i = 0; (s = di->bits.ar.inits[i]); i++){
+				if(s == DYNARRAY_NULL){
+					stylef("0");
+				}else if(s->type == decl_init_copy){
+					stylef("<copy from store %ld>", DECL_INIT_COPY_IDX(s, di));
+				}else{
+					gen_style_dinit(s);
+				}
+
+				if(di->bits.ar.inits[i + 1])
+					stylef(",\n");
+			}
+
+			stylef("}");
+		}
+	}
 }
 
 void gen_style_decl(decl *d)
