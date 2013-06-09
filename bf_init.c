96189b1f91ec0dbb2686fc349951fd2d6a81177e
Rob Pilling <robpilling@gmail.com>
2013-06-09 15:02:19 +0100

    bf_init changes

--- bf_init.c
+++ bf_init.c
@@ -1,12 +1,24 @@
-struct A
+struct Basic
+{
+	int x : 4, y : 4;
+} bas = { 1, 2 };
+
+struct Desig
+{
+	int x : 16, y : 2;
+} des = {
+	.y = 1
+};
+
+struct Padded
 {
 	int i : 2;
 	int : 0;
 	int j : 3;
 	int : 4;
-	int end : 7;
+	int end : 13;
 };
 
-struct A x = {
-	1, 2, 3; // should initialise i, j and k, skipping unnamed fields
+struct Padded pad = {
+	1, 2, 3 // should initialise i, j and k, skipping unnamed fields
 };
