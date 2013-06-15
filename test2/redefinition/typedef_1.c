8741e33493a196ada008228ace3dd67be67db887
Rob Pilling <robpilling@gmail.com>
2013-06-15 10:52:26 +0100

    Moved redefinition tests

--- test2/redefinition/typedef_1.c
+++ test2/redefinition/typedef_1.c
@@ -1,3 +1,4 @@
+// RUN: %ucc -c %s
 typedef unsigned long size_t;
 
 f()
@@ -6,20 +7,4 @@ f()
 	size_t x;
 
 	_Static_assert(sizeof(x) == 2, "short typedef missed");
-}
-
-g()
-{
-	size_t a;
-	typedef int a; // CHECK: /error: redef/
-}
-
-h()
-{
-	typedef int t;
-	{
-		int t;
-		t = 2;
-		return t;
-	}
 }
