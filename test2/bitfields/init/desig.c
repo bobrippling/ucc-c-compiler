cffc6bdbc55de868cabd4a8b683755e8e00a5fb1
Rob Pilling <robpilling@gmail.com>
2013-06-19 22:45:03 +0100

    Finished bitfields

--- test2/bitfields/init/desig.c
+++ test2/bitfields/init/desig.c
@@ -1,5 +1,5 @@
 // RUN: %ucc -o %t %s
-// RUN: %t | %output_check '65536'
+// RUN: %t | %output_check '65536' '-1 -2' '11'
 
 struct Desig
 {
@@ -8,7 +8,20 @@ struct Desig
 	.y = 1
 };
 
+struct A
+{
+	int x : 2, y : 2;
+} yo = {
+	.y = 2,
+	.x = 3
+};
+
 main()
 {
 	printf("%d\n", *(int *)&des);
+
+	struct A st = { .y = 2, .x = 3 };
+	printf("%d %d\n", st.x, st.y);
+
+	printf("%d\n", 0xf & *(char *)&yo);
 }
