69a4184319abc39aca94eee5551422e5721820d7
Rob Pilling <robpilling@gmail.com>
2013-08-18 17:18:01 +0100

    Updated struct late definition test

--- test2/structs/scope/late_def.c
+++ test2/structs/scope/late_def.c
@@ -5,8 +5,8 @@ struct A;
 int f()
 {
 	struct A a; // CHECK: /error: .*incomplete/
-	a.i = 2;
-	return a.i;
+	//a.i = 2;
+	//return a.i;
 }
 
 struct A // this doesn't complete struct A for f()'s scope
