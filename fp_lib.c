420f4bc35b7330429fbe1d2ae7c6edb289caa1dd
Rob Pilling <robpilling@gmail.com>
2013-08-22 18:48:05 +0100

    Simpler test output for fp_lib/funcall

--- fp_lib.c
+++ fp_lib.c
@@ -1,5 +1,5 @@
 g(float a, float b, float c, float d, float e, float f, float g, float h, float i)
 {
-	printf("%f %f %f %f %f %f %f %f %f %f\n",
+	printf("%.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f\n",
 			a, b, c, d, e, f, g, h, i);
 }
