724b5631f2466466d9f2a9541fd5f3a68a9f2589
Rob Pilling <robpilling@gmail.com>
2013-08-22 18:48:04 +0100

    Added float/variadic examples

---
+++ fp_vari.simple.c
@@ -0,0 +1,17 @@
+double f(int a, ...)
+{
+	__builtin_va_list l;
+
+	__builtin_va_start(l, a);
+
+	double r = __builtin_va_arg(l, double);
+
+	__builtin_va_end(l);
+
+	return r;
+}
+
+main()
+{
+	printf("%f\n", f(2, (float)3));
+}
