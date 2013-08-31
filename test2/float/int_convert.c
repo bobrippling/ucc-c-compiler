0259179ecfa11bbc3f5a409e9578378597748bb7
Rob Pilling <robpilling@gmail.com>
2013-08-31 16:22:42 +0100

    Added explicit conversion check for LL+fp -> fp

--- test2/float/int_convert.c
+++ test2/float/int_convert.c
@@ -2,6 +2,12 @@
 // RUN: [ `%ucc -S -o- %s | grep 'cvtsi2ssq ' | wc -l` -eq 1 ]
 // RUN: %ucc -c %s
 
+_Static_assert(
+		__builtin_types_compatible_p(
+			__typeof(5LL + 1.0f),
+			float),
+		"long long + float should be float");
+
 float l_to_float(long long l)
 {
 	return l;
