e3cfc2b8690cb6fbc4a9b15c8730c43d3a8efc50
Rob Pilling <robpilling@gmail.com>
2013-09-12 22:39:23 +0100

    Don't pass -g to the assembler

--- src/ucc/ucc.c
+++ src/ucc/ucc.c
@@ -461,7 +461,7 @@ arg_ld:
 					if(argv[i][2])
 						die("-g... unexpected");
 					ADD_ARG(mode_compile);
-					ADD_ARG(mode_assemb);
+					/* don't pass to the assembler */
 					continue;
 
 				case 'M':
