6803a916a460bb800f8886f27292be02ffc38c2c
Rob Pilling <robpilling@gmail.com>
2013-09-27 19:42:10 +0100

    cpp: pulled out read_line code from splice_line()

--- src/cpp2/preproc.c
+++ src/cpp2/preproc.c
@@ -110,66 +110,66 @@ static void preproc_pop(void)
 	preproc_out_info();
 }
 
-static char *splice_line(void)
+static char *read_line(void)
+{
+	FILE *f;
+	char *line;
+
+re_read:
+	if(file_stack_idx < 0)
+		ICE("file stack idx = 0 on read()");
+	f = file_stack[file_stack_idx].file;
+	line = fline(f);
+
+	if(!line){
+		if(ferror(f))
+			die("read():");
+
+		fclose(f);
+		if(file_stack_idx > 0){
+			free(dirname_pop());
+			preproc_pop();
+			goto re_read;
+		}
+
+		return NULL;
+	}
+
+	current_line++;
+
+	return line;
+}
+
+static char *splice_lines(void)
 {
 	static int n_nls;
-	char *last;
-	int join;
+	char *line;
+	size_t len;
 
 	if(n_nls){
 		n_nls--;
 		return ustrdup("");
 	}
 
-	last = NULL;
-	join = 0;
+	line = read_line();
+	if(!line)
+		return NULL;
 
-	for(;;){
-		FILE *f;
-		int len;
-		char *line;
+	len = strlen(line);
 
-re_read:
-		if(file_stack_idx < 0)
-			ICE("file stack idx = 0 on read()");
-		f = file_stack[file_stack_idx].file;
-		line = fline(f);
+	if(len && line[len - 1] == '\\'){
+		char *next = splice_lines();
+		const size_t next_len = strlen(next);
 
-		if(!line){
-			if(ferror(f))
-				die("read():");
+		n_nls++;
+		line[len - 1] = '\0';
 
-			fclose(f);
-			if(file_stack_idx > 0){
-				free(dirname_pop());
-				preproc_pop();
-				goto re_read;
-			}
-
-			return NULL;
-		}
-
-		current_line++;
-
-		if(join){
-			join = 0;
-
-			last = urealloc1(last, strlen(last) + strlen(line) + 1);
-			strcpy(last + strlen(last), line);
-			free(line);
-			line = last;
-		}
-
-		len = strlen(line);
-		if(len && line[len - 1] == '\\'){
-			line[len - 1] = '\0';
-			join = 1;
-			last = line;
-			n_nls++;
-		}else{
-			return line;
-		}
+		line = urealloc1(line, len + next_len + 1);
+		strcpy(line + len - 1, next);
+		free(next);
 	}
+
+	return line;
 }
 
 static char *strip_comment(char *line)
@@ -267,18 +267,17 @@ void preprocess(void)
 
 	preproc_push(stdin, current_fname);
 
-	while((line = splice_line())){
-		char *s;
+	while((line = splice_lines())){
 		debug_push_line(line);
 
-		s = filter_macros(strip_comment(line));
+		line = filter_macros(strip_comment(line));
 
 		debug_pop_line();
 
-		if(s){
+		if(line){
 			if(!no_output)
-				puts(s);
-			free(s);
+				puts(line);
+			free(line);
 		}
 	}
 
