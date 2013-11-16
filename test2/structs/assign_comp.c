86ddfa2c65cf785de68ce05d5404325e3186f7ca
Rob Pilling <robpilling@gmail.com>
2013-11-16 12:35:13 +0000

    Added struct assign from literal test

---
+++ test2/structs/assign_comp.c
@@ -0,0 +1,22 @@
+// RUN: %ocheck 0 %s
+
+struct A
+{
+	int i, j, k;
+};
+
+f(struct A *p)
+{
+	*p = (struct A){ 1, .k = 3 };
+}
+
+main()
+{
+	struct A a;
+	f(&a);
+
+	if(a.i != 1 || a.j != 0 || a.k != 3)
+		abort();
+
+	return 0;
+}
