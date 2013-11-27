c4de8b719e7e2eb969f2ac6f26e0e4006a47954f
Rob Pilling <robpilling@gmail.com>
2013-11-27 20:47:09 +0000

    Test fix

--- test2/initialisation/compound_global/yes_no_cases.c
+++ test2/initialisation/compound_global/yes_no_cases.c
@@ -2,15 +2,14 @@
 
 int (*p)[] = &(int[]){1, 2, 3}; // CHECK: !/error:/
 
-int   a[]  =  (int[]){1, 2, 3}; // CHECK: /error:/
-int (*p)[] = &(int[]){1, 2, 3}; // CHECK: !/error:/
-int  *p[]  = &(int[]){1, 2, 3}; // CHECK: /error:/
+int (*q)[] = &(int[]){1, 2, 3}; // CHECK: !/error:/
+int  *r[]  = &(int[]){1, 2, 3}; // CHECK: /error:/
 
 f()
 {
-	static int   a[] = (int[]){1, 2, 3}; // CHECK: /error:/
 	static char  b[] = (int[]){1, 2, 3}; // CHECK: /error:/
-	       char  c[] = (int[]){1, 2, 3}; // CHECK: /error:/
 	        char  *d = (int[]){1, 2, 3}; // CHECK: !/error:/
 	static char   *e = (int[]){1, 2, 3}; // CHECK: !/error:/
 }
+
+int a[] = (int[]){1, 2, 3}; // CHECK: /error:/
