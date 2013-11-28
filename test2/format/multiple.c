f88a2245eed7a83b7ac3bd546532ecc961ed9832
Rob Pilling <robpilling@gmail.com>
2013-11-28 22:11:21 +0000

    Format test cleanup

--- test2/format/multiple.c
+++ test2/format/multiple.c
@@ -2,17 +2,17 @@
 
 int printf(const char *, ...) __attribute__((format(printf, 1, 2))); // CHECK: !/warn|err/
 
-not_variadic(char *) __attribute__((format(printf, 1, 2))); // CHECK: /error: "f2" is not a variadic function/
+not_variadic(char *) __attribute__((format(printf, 1, 2))); // CHECK: /warning: variadic function required for format attribute/
 
 just_str_check(char *, ...) __attribute((format(printf, 1, 0))); // CHECK: !/warn|err/
 
-bad_v_idx(int, int, char *, ...) __attribute__((format(printf, 3, 3))); // CHECK: /error: variadic argument 3 not '...'/
+bad_v_idx(int, int, char *, ...) __attribute__((format(printf, 3, 3))); // CHECK: warning: variadic argument out of bounds (should be 4)
 
 bad_v_idx2(int, int, int, int, char *, ...)
-	__attribute__((format(printf, 5, 4))); // CHECK: /error: variadic argument (4) before format argument (5)/
+	__attribute__((format(printf, 5, 4))); // CHECK: warning: variadic argument out of bounds (should be 6)
 
 bad_fmt_idx(char *, int, ...)
-	__attribute__((format(printf, 2, 3))); // CHECK: /error: format index not 'char *'/
+	__attribute__((format(printf, 2, 3))); // CHECK: warning: format argument not a string type
 
 main()
 {
@@ -27,5 +27,7 @@ main()
 
 	bad_v_idx(1, 2, "hi %s", 3); // CHECK: !/warn/
 	bad_v_idx2(5, 5, 5, 5, "yo %d", "hi"); // CHECK: !/warn/
+
+	// shouldn't get warnings on functions with bad format(printf,...) specs:
 	bad_fmt_idx("hi %s", 3, 5); // CHECK: !/warn/
 }
