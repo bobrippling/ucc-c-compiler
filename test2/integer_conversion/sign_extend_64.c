b265b3e65481b5b7b164b813bfcff3a81bacf223
Rob Pilling <robpilling@gmail.com>
2013-12-03 21:15:19 +0000

    intval loading example(s)

--- test2/integer_conversion/sign_extend_64.c
+++ test2/integer_conversion/sign_extend_64.c
@@ -4,8 +4,8 @@ typedef unsigned long long uint64_t;
 
 int main()
 {
-	uint64_t x = 1 << 31;
-	uint64_t y = (uint64_t)1 << 31;
+	uint64_t x = 1 << 31; // (int)1 << 31 = -1, sign extend up
+	uint64_t y = (uint64_t)1 << 31; // -1ULL << 31 = 0x8000.., zextend up
 
 	if(x != 0xffffffff80000000)
 		abort();
