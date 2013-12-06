86d43143c1da5ca7482cba89259555c7cc3ce564
Rob Pilling <robpilling@gmail.com>
2013-12-06 23:02:50 +0000

    expr.h indentation

--- src/cc1/expr.h
+++ src/cc1/expr.h
@@ -50,23 +50,23 @@ typedef struct consty
 #define CONST_FOLD_LEAF(k) memset((k), 0, sizeof *(k))
 
 
-typedef void         func_fold(          expr *, symtable *);
-typedef void         func_gen(           expr *);
-typedef void         func_gen_lea(       expr *);
-typedef void         func_const(         expr *, consty *);
-typedef const char  *func_str(void);
-typedef void         func_mutate_expr(expr *);
+typedef void func_fold(expr *, symtable *);
+typedef void func_gen(expr *);
+typedef void func_gen_lea(expr *);
+typedef void func_const(expr *, consty *);
+typedef const char *func_str(void);
+typedef void func_mutate_expr(expr *);
 
 struct expr
 {
 	where where;
 
-	func_fold        *f_fold;
-	func_gen         *f_gen;
-	func_str         *f_str;
+	func_fold *f_fold;
+	func_gen *f_gen;
+	func_str *f_str;
 
-	func_const       *f_const_fold; /* optional, used in static/global init */
-	func_gen_lea     *f_lea;        /* optional */
+	func_const *f_const_fold; /* optional, used in static/global init */
+	func_gen_lea *f_lea; /* optional */
 
 
 	int freestanding; /* e.g. 1; needs use, whereas x(); doesn't - freestanding */
