3fc1df505a699ffce49baec115786a5652df19af
Rob Pilling <robpilling@gmail.com>
2013-12-06 21:49:03 +0000

    Allow void expression lhs/rhs in comma expressions

--- src/cc1/ops/expr_comma.c
+++ src/cc1/ops/expr_comma.c
@@ -25,10 +25,16 @@ static void fold_const_expr_comma(expr *e, consty *k)
 void fold_expr_comma(expr *e, symtable *stab)
 {
 	FOLD_EXPR(e->lhs, stab);
-	fold_check_expr(e->lhs, FOLD_CHK_NO_ST_UN, "comma-expr");
+	fold_check_expr(
+			e->lhs,
+			FOLD_CHK_NO_ST_UN | FOLD_CHK_ALLOW_VOID,
+			"comma-expr");
 
 	FOLD_EXPR(e->rhs, stab);
-	fold_check_expr(e->rhs, FOLD_CHK_NO_ST_UN, "comma-expr");
+	fold_check_expr(
+			e->rhs,
+			FOLD_CHK_NO_ST_UN | FOLD_CHK_ALLOW_VOID,
+			"comma-expr");
 
 	e->tree_type = e->rhs->tree_type;
 
