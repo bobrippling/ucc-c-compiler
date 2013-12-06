62f6c3e0e218edd4460cd851e2a574d74efa6965
Rob Pilling <robpilling@gmail.com>
2013-12-06 21:49:05 +0000

    Test warning fixes

--- test2/redefinition/static_mismatch.c
+++ test2/redefinition/static_mismatch.c
@@ -1,13 +1,13 @@
 // RUN: %check -e %s
 
-int f(); // CHECK: /error: mismatching definitions of "f"/
+int f(); // CHECK: /note: previous definition/
 
 static int caller()
 {
 	return f();
 }
 
-static int f() // CHECK: /note: other definition/
+static int f() // CHECK: /error: mismatching definitions of "f"/
 {
 	return 3;
 }
