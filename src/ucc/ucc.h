dcf22d5a62a0f648f0308f06c94f94aa398f1019
Rob Pilling <robpilling@gmail.com>
2013-12-13 22:22:30 +0000

    Stronger type checking on die()

--- src/ucc/ucc.h
+++ src/ucc/ucc.h
@@ -1,9 +1,11 @@
 #ifndef UCC_H
 #define UCC_H
 
+#include "../util/compiler.h"
+
 extern const char *argv0;
 extern char *wrapper; /* -wrapper gdb,--args */
 extern char *Bprefix; /* -Bexec-prefix */
-void die(const char *s, ...);
+void die(const char *s, ...) ucc_printflike(1, 2);
 
 #endif
