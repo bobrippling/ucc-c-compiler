2a9fb99f2f8c0b5265ccb2aad52e13597a3ac6a2
Rob Pilling <robpilling@gmail.com>
2013-12-15 11:59:28 +0000

    Updated struct_todo.c

--- struct_todo.c
+++ struct_todo.c
@@ -1,13 +1,19 @@
+struct A
+{
+	int i, j, k;
+};
+
 f(struct A);
 struct A get();
 
 main()
 {
+	struct A y = {}, z = {};
 	struct A x = pred() ? y : z;
 
 	struct A cst = (struct A)x;
 
-	struct A comma = cst, x;
+	struct A comma = (cst, x);
 
 	struct A exp_stmt = ({ struct A sub = { 1 }; sub; });
 
