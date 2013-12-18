a9d1ecd600f61bd11ee5ccff14a731240b4af16b
Rob Pilling <robpilling@gmail.com>
2013-12-18 21:16:50 +0000

    More logging in struct_todo.c

--- struct_todo.c
+++ struct_todo.c
@@ -6,18 +6,32 @@ struct A
 f(struct A);
 struct A get();
 
+pred(){ return 1; }
+
+print_A(struct A const *p)
+{
+	printf("{ %d, %d, %d }\n", p->i, p->j, p->k);
+}
+
 main()
 {
 	struct A y = {}, z = {};
 	struct A x = pred() ? y : z;
 
-	//struct A cst = (struct A)x;
+	/*struct A cst = (struct A)x;*/
 
-	struct A comma = (y, x);
+	struct A comma = (y, z);
 
-	struct A exp_stmt = ({ struct A sub = { 1 }; sub; });
+	//struct A exp_stmt = ({ struct A sub = { .k = 1 }; sub; });
 
-	struct A init = (struct A){ 1 };
+	struct A init = (struct A){ .j = 1 };
 
-	(void)x;
+	//(void)x;
+
+	print_A(&y);
+	print_A(&z);
+	print_A(&x);
+	print_A(&comma);
+	//print_A(&exp_stmt);
+	print_A(&init);
 }
