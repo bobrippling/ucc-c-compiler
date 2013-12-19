37995a22bbc096b120ff87b7783fd70113f85c14
Rob Pilling <robpilling@gmail.com>
2013-12-19 18:50:29 +0000

    Avoid printf() in struct lvalue test

--- test2/structs/lvalue/operations.c
+++ test2/structs/lvalue/operations.c
@@ -1,41 +1,47 @@
-// RUN: %ucc -o %t %s
-// RUN: %ocheck 0 %t
-// RUN: %t | %output_check '{ 0, 0, 0, y }' '{ 0, 0, 0, z }' '{ 0, 0, 0, y }' '{ 0, 0, 0, z }' '{ 0, 0, 1, s }' '{ 0, 1, 0, i }'
+// RUN: %ocheck 0 %s
 
 struct A
 {
 	int i, j, k;
-	char ch;
+	char c;
 };
 
 pred(){ return 1; }
 
-print_A(struct A const *p)
+check_A(
+		struct A const *p,
+		int i, int j, int k, char c)
 {
-	printf("{ %d, %d, %d, %c }\n", p->i, p->j, p->k, p->ch);
+	if(p->i != i
+	|| p->j != j
+	|| p->k != k
+	|| p->c != c)
+	{
+		abort();
+	}
 }
 
 main()
 {
-	struct A y = { .ch = 'y' }, z = { .ch = 'z' };
+	struct A y = { .c = 'y' }, z = { .c = 'z' };
 	struct A x = pred() ? y : z;
 
 	/*struct A cst = (struct A)x;*/
 
 	struct A comma = (y, z);
 
-	struct A exp_stmt = ({ struct A sub = { .k = 1, .ch = 's' }; sub; });
+	struct A exp_stmt = ({ struct A sub = { .k = 1, .c = 's' }; sub; });
 
-	struct A init = (struct A){ .j = 1, .ch = 'i' };
+	struct A init = (struct A){ .j = 1, .c = 'i' };
 
 	(void)x;
 
-	print_A(&y);
-	print_A(&z);
-	print_A(&x);
-	print_A(&comma);
-	print_A(&exp_stmt);
-	print_A(&init);
+	check_A(&y,        0, 0, 0, 'y');
+	check_A(&z,        0, 0, 0, 'z');
+	check_A(&x,        0, 0, 0, 'y');
+	check_A(&comma,    0, 0, 0, 'z');
+	check_A(&exp_stmt, 0, 0, 1, 's');
+	check_A(&init,     0, 1, 0, 'i');
 
 	return 0;
 }
