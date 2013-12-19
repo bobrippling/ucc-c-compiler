bf54c612a667794b8d4044c0fb26c349ecd062a4
Rob Pilling <robpilling@gmail.com>
2013-12-19 18:54:11 +0000

    Document out_memcpy()

--- src/cc1/out/out.h
+++ src/cc1/out/out.h
@@ -31,7 +31,7 @@ void out_deref(void);
 void out_swap(void);
 void out_flush_volatile(void);
 
-void out_memcpy(unsigned long bytes);
+void out_memcpy(unsigned long bytes); /* copies *vtop[0] -> *vtop[1] */
 
 void out_cast(type_ref *to) ucc_nonnull((1));
 void out_change_type(type_ref *) ucc_nonnull((1));
