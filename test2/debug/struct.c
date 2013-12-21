03c258eee4bad925c985d63d09ea110dfc0843b2
Rob Pilling <robpilling@gmail.com>
2013-12-21 19:45:06 +0000

    Debug layout test for structs

---
+++ test2/debug/struct.c
@@ -0,0 +1,26 @@
+// RUN: %layout_check %s -g
+
+typedef float fp;
+
+typedef struct A
+{
+	char c;
+	short s;
+	char pad;
+	int i;
+	long l;
+	char *p;
+	char buf[5], buf2[6];
+	fp f, fs[2];
+	double d;
+} A;
+
+A x = {
+	.p = "hello",
+	.s = 5,
+	.buf = "hello",
+	.buf2 = "hello",
+	.d = 3.251,
+	.f = 59.7,
+	.fs[1] = { 5 }
+};
