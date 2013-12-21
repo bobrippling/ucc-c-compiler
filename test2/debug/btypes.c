25c6e72f9c494d58831299db5f41df7b7880b856
Rob Pilling <robpilling@gmail.com>
2013-12-21 19:46:22 +0000

    Debug layout test for basic types

---
+++ test2/debug/btypes.c
@@ -0,0 +1,20 @@
+// RUN: %layout_check %s -g
+
+signed char sc = 'a';
+unsigned char uc = 'b';
+
+signed short ss = 1;
+unsigned short us = 2;
+
+signed int si = 3;
+unsigned int ui = 4;
+
+signed long sl = 5;
+unsigned long ul = 6;
+
+signed long long sll = 7;
+unsigned long long ull = 8;
+
+float f = 1.2f;
+double d = 3.4f;
+//long double ld;
