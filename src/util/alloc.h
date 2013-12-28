a95dbc555528e9e281715cd2decc951caaef360d
Rob Pilling <robpilling@gmail.com>
2013-12-28 13:59:37 +0000

    Type check for ustrprintf()

--- src/util/alloc.h
+++ src/util/alloc.h
@@ -1,12 +1,14 @@
 #ifndef ALLOC_H
 #define ALLOC_H
 
+#include "compiler.h"
+
 void *umalloc(size_t);
 void *urealloc1(void *, size_t new);
 void *urealloc(void *, size_t new, size_t old);
 char *ustrdup(const char *);
 char *ustrdup2(const char *, const char *b); /* up to, but not including b */
-char *ustrprintf( const char *, ...);
+char *ustrprintf( const char *, ...) ucc_printflike(1, 2);
 char *ustrvprintf(const char *, va_list);
 
 #endif
