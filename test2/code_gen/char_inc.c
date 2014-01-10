ec1c9003c3999fc1cd24e52d91676b2cce0919eb
Rob Pilling <robpilling@gmail.com>
2014-01-10 23:40:43 +0000

    Moved fixed char-increment bug

--- test2/code_gen/char_inc.c
+++ test2/code_gen/char_inc.c
@@ -1,10 +1,17 @@
+// RUN: %ocheck 0 %s
+
 main()
 {
 	for(unsigned char c = 1; c > 0; c++)
-		printf("%d\n", c);
+		;
 
 	char c = 3;
 
-	c += (int)1; // FIXME: this should generate addl, not addb
+	c += (int)1; // this should generate addl, not addb
 	// i.e. integer promotion rules (for compound assign)
+
+	if(c != 4)
+		abort();
+
+	return 0;
 }
