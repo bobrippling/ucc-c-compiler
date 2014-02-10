b70651963e777953cd16fca6157f58097b1409de
Rob Pilling <robpilling@gmail.com>
2014-02-10 20:59:57 +0000

    Skip casts when logging about unused experssions

--- src/cc1/ops/stmt_expr.c
+++ src/cc1/ops/stmt_expr.c
@@ -12,9 +12,14 @@ const char *str_stmt_expr()
 void fold_stmt_expr(stmt *s)
 {
 	FOLD_EXPR(s->expr, s->symtab);
-	if(!s->freestanding && !s->expr->freestanding && !type_is_void(s->expr->tree_type))
+
+	if(!s->freestanding
+	&& !s->expr->freestanding
+	&& !type_is_void(s->expr->tree_type))
+	{
 		cc1_warn_at(&s->expr->where, 0, WARN_UNUSED_EXPR,
-				"unused expression (%s)", s->expr->f_str());
+				"unused expression (%s)", expr_skip_casts(s->expr)->f_str());
+	}
 }
 
 void gen_stmt_expr(stmt *s)
