8bc6d733c9831f6c75141061cdf8f3d8ba927c9f
Rob Pilling <robpilling@gmail.com>
2014-03-09 17:50:56 +0000

    Added checks to if_expr_code_gen.c

--- if_expr_code_gen.c
+++ if_expr_code_gen.c
@@ -1,10 +1,29 @@
+// RUN: %ocheck 0 %s
+
 struct A
 {
 	struct A *next;
 };
 
-struct A *f(struct A *p)
+struct A *next(struct A *p)
 {
 	// we are casting this to void and messing with the phi-logic
 	return p ? p->next : 0;
+}
+
+main()
+{
+	struct A self = { &self };
+	struct A a = { 0 }, b = { &a };
+
+	if(next(&self) != &self)
+		abort();
+	if(next(&a) != 0)
+		abort();
+	if(next(&b) != &a)
+		abort();
+	if(next(0) != 0)
+		abort();
+
+	return 0;
 }
