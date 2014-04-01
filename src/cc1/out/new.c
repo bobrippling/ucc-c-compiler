74eae3f9480e8b7a3cee8e2e37736514a429a784
Rob Pilling <robpilling@gmail.com>
2014-04-02 00:01:55 +0100

    TODO() out_new_*()

--- src/cc1/out/new.c
+++ src/cc1/out/new.c
@@ -6,62 +6,76 @@
 #include "out.h" /* this file defs */
 #include "val.h"
 
+#include <stdio.h>
+#define TODO() fprintf(stderr, "TODO: %s\n", __func__)
+
 out_val *out_new_blk_addr(out_ctx *octx, out_blk *blk)
 {
+	TODO();
 	return 0;
 }
 
 out_val *out_new_frame_ptr(out_ctx *octx, int nframes)
 {
+	TODO();
 	return 0;
 }
 
 out_val *out_new_l(out_ctx *octx, type *ty, long val)
 {
+	TODO();
 	return 0;
 }
 
 out_val *out_new_lbl(out_ctx *octx, const char *s, int pic)
 {
+	TODO();
 	return 0;
 }
 
 out_val *out_new_nan(out_ctx *octx, type *ty)
 {
+	TODO();
 	return 0;
 }
 
 out_val *out_new_noop(out_ctx *octx)
 {
+	TODO();
 	return 0;
 }
 
 out_val *out_new_num(out_ctx *octx, type *t, const numeric *n)
 {
+	TODO();
 	return 0;
 }
 
 out_val *out_new_overflow(out_ctx *octx)
 {
+	TODO();
 	return 0;
 }
 
 out_val *out_new_reg_save_ptr(out_ctx *octx)
 {
+	TODO();
 	return 0;
 }
 
 out_val *out_new_sym(out_ctx *octx, sym *sym)
 {
-	return 0;
+	TODO();
 }
 
 out_val *out_new_sym_val(out_ctx *octx, sym *sym)
 {
+	TODO();
 	return 0;
 }
 
 out_val *out_new_zero(out_ctx *octx, type *ty)
 {
+	TODO();
 	return 0;
 }
