0ea03803e1c516db141bee7dab4c474c24a88e8f
Rob Pilling <robpilling@gmail.com>
2014-05-06 00:11:14 +0100

    Several constant expression + addr constant examples

---
+++ const_ptr_arith.c
@@ -0,0 +1,21 @@
+// test cmp, addition and (int & ptr) subtraction
+
+// equal, 1
+cmp = &((int *)0)[5] == 5 * sizeof(int);
+
+// 12 + 2 * sizeof(int) = 20
+add = &((int *)0)[3] + 2;
+
+// 16 sub 2 * sizeof(int) = 8
+dif = &((int *)0)[4] - 2;
+
+// 40 - 8 = 32, div sizeof(int) = 8
+sub = &((int *)0)[10] - (int *)8;
+
+main()
+{
+	if(cmp != 1) abort();
+	if(add != 20) abort();
+	if(dif != 8) abort();
+	if(sub != 8) abort();
+}
---
+++ struct-assert.c
@@ -0,0 +1,58 @@
+//#include <stddef.h>
+//#define offsetof(t, m) (unsigned long)(&((t *)0)->m)
+
+/*
+#ifdef __clang__
+#  warning clang static assert
+#  define JOIN_(x, y) x ## y
+#  define JOIN(x, y) JOIN_(x, y)
+#  define _Static_assert(x, s) char JOIN(__yo, __LINE__)[(x) && s ? 1 : -1]
+#endif
+*/
+
+//#define OFF 4
+struct __attribute__((/*packed*/)) B
+{
+	char a;
+	int i;
+};
+
+struct A
+{
+	struct B b;
+	_Static_assert(
+			//offsetof(struct B, i) == 1,
+			(unsigned long)(&((struct B *)0)->i) == 4, // NOT CONST
+			"S not packed");
+};
+
+int glob;
+
+/* &glob == &glob isn't an integer constant expression,
+ * but is an address constant expression, so may be used
+ * in an initialiser */
+_Static_assert(&glob == &glob, ""); // !cosnt
+
+int x = &glob > &glob; // CHECK: !/warn/
+
+_Static_assert(&glob != 0, "");
+
+_Static_assert(
+		&((int *)0)[5] == 5 * sizeof(int),
+		"");
+
+main()
+{
+	if((unsigned long)(&((struct B *)0)->i) != 4)
+		abort();
+
+	_Static_assert(
+			(unsigned long)(&((struct B *)0)->i) // !const
+			==
+			(unsigned long)(&((struct B *)0)->i),
+			"");
+
+	_Static_assert(
+			&((struct B *)0)->i == &((struct B *)0)->i, // !const
+			"");
+}
