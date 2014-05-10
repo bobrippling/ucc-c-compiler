35c374d8eb817fedc610877ff47212c701fbaf28
Rob Pilling <robpilling@gmail.com>
2014-05-10 12:17:57 +0100

    Don't import argument symtables in function prototypes

--- test2/redefinition/prototype_scope.c
+++ test2/redefinition/prototype_scope.c
@@ -1,3 +1,6 @@
+// RUN: %ucc -fsyntax-only %s
+
+// check the ctx arguments don't clash:
 int a(void *ctx, void cb(void *ctx));
 int b(void cb(void *ctx), void *ctx);
 
@@ -6,9 +9,4 @@ f(int x)
 	{
 		int x = 3;
 	}
-}
-
-g(int x)
-{
-	int x = 3;
 }
