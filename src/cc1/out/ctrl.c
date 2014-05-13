67577e888d13df0116f5dba3d23b5e502a5bb164
Rob Pilling <robpilling@gmail.com>
2014-05-13 23:13:51 +0100

    out_ctrl_merge() - phi block merging

--- src/cc1/out/ctrl.c
+++ src/cc1/out/ctrl.c
@@ -11,6 +11,7 @@
 #include "val.h"
 #include "impl.h"
 #include "ctx.h"
+#include "virt.h"
 
 static void register_block(out_ctx *octx, out_blk *blk)
 {
@@ -47,12 +48,31 @@ void out_ctrl_end_undefined(out_ctx *octx)
 
 out_val *out_ctrl_merge(out_ctx *octx, out_blk *from_a, out_blk *from_b)
 {
-	/* maybe ret null
-	 * here we set merge_preds */
-	(void)octx;
-	(void)from_a;
-	(void)from_b;
-	return NULL;
+	/* here we set merge_preds */
+	out_blk *const saved_current_blk = octx->current_blk;
+	type *ty;
+	struct vreg merge_reg;
+
+	assert(from_a->phi_val && from_b->phi_val);
+
+	ty = from_a->phi_val->t;
+
+	/* need them both in a register */
+	out_current_blk(octx, from_a);
+	{
+		out_val *regged = v_to_reg_out(octx, from_a->phi_val, &merge_reg);
+		out_flush_volatile(octx, regged);
+		out_comment(octx, "FLUSH TO REG %d", merge_reg.idx);
+	}
+	out_current_blk(octx, from_b);
+	{
+		out_val *regged = v_to_reg_given(octx, from_b->phi_val, &merge_reg);
+		out_flush_volatile(octx, regged);
+	}
+
+	out_current_blk(octx, saved_current_blk);
+
+	return v_new_reg(octx, NULL, ty, &merge_reg);
 }
 
 void out_current_blk(out_ctx *octx, out_blk *new_blk)
