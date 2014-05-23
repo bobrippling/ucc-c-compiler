88b85c3c5aff809d32c764c21b838ea4ea637030
Rob Pilling <robpilling@gmail.com>
2014-05-23 17:39:42 +0100

    Fix no-op dereference of functions

--- src/cc1/out/out.c
+++ src/cc1/out/out.c
@@ -190,7 +190,7 @@ const out_val *out_deref(out_ctx *octx, const out_val *target)
 
 	/* if the pointed-to object is not an lvalue, don't deref */
 	if(type_is(tnext, type_array)
-	|| type_is(type_is_ptr(tnext), type_func))
+	|| type_is(tnext, type_func))
 	{
 		/* noop */
 		return v_dup_or_reuse(octx, target, tnext);
