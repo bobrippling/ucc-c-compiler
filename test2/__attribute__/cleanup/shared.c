0b5fef1145ad399a3522ba445f0fa7869b7ba00c
Rob Pilling <robpilling@gmail.com>
2014-06-15 18:27:25 +0100

    Additional cleanup tests - array and shared_ptr

---
+++ test2/__attribute__/cleanup/shared.c
@@ -0,0 +1,54 @@
+// RUN: %ocheck 0 %s
+
+extern void *malloc(int);
+extern void free(void *);
+
+int freed;
+
+struct A
+{
+	unsigned retains;
+	void (*dealloc)(struct A *);
+	char *mem;
+};
+
+void decref_a(struct A **pp)
+{
+	struct A *p = *pp;
+
+	if(--p->retains)
+		return;
+
+	p->dealloc(p);
+	free(p);
+}
+
+typedef __attribute((cleanup(decref_a))) struct A shared_a;
+
+struct A *f()
+{
+	shared_a *p = malloc(sizeof *p);
+
+	p->retains = 2; /* this and caller */
+
+	return p;
+}
+
+void show_a_dealloc(struct A *p)
+{
+	freed = 1;
+}
+
+g()
+{
+	shared_a *p = f();
+	p->dealloc = show_a_dealloc;
+}
+
+main()
+{
+	g();
+	if(!freed)
+		abort();
+	return 0;
+}
