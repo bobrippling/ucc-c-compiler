85cc1e9a7057f72434ea12fbc6f24b771e8c435c
Rob Pilling <robpilling@gmail.com>
2014-06-28 00:32:53 +0100

    Move vla_bad_parse.c

---
+++ test2/vla/bad_parse.c
@@ -0,0 +1,8 @@
+// RUN: %check -e %s
+
+int bad(char ch[static *]); // CHECK: error: 'static' can't be used with a star-modified array
+
+void bad2(char buf[*]) // CHECK: error: star modifier can only appear on prototypes
+{
+	(void)buf;
+}
