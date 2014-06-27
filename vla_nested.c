8da882ce980d8db0db4b7456cd73f8ed4bce7042
Rob Pilling <robpilling@gmail.com>
2014-06-27 23:11:48 +0100

    VLA nested - evaluation checking

--- vla_nested.c
+++ vla_nested.c
@@ -1,4 +1,28 @@
-f(int n)
+#define L() printf("%s\n", __func__)
+
+typedef void any(int);
+typedef int ir(void);
+
+any a, b, c;
+ir x, y;
+
+void a(int x){L();}
+void b(int x){L();}
+void c(int x){L();}
+
+int x(){L();return 1;}
+int y(){L();return 1;}
+
+void f()
 {
-	int x[n][n];
+	int ar[x()][y()];
+
+	a(sizeof ar);
+	b(sizeof *ar);
+	c(sizeof **ar);
+}
+
+main()
+{
+	f();
 }
