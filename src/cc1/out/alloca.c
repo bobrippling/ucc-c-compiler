80d8d089d256156781b1ce5db731971f84ca43d1
Rob Pilling <robpilling@gmail.com>
2014-07-03 22:39:46 +0100

    Align the stack on VLA allocation

--- src/cc1/out/alloca.c
+++ src/cc1/out/alloca.c
@@ -2,6 +2,11 @@
 
 #include "out.h"
 #include "val.h"
+#include "../type_nav.h"
+
+/* cc1_mstack_align */
+#include <stdarg.h>
+#include "../cc1.h"
 
 static const out_val *alloca_stack_adj(
 		out_ctx *octx, enum op_type op,
@@ -18,20 +23,29 @@ static const out_val *alloca_stack_adj(
 	return adj;
 }
 
-#include <stdio.h>
-
 const out_val *out_alloca_push(
 		out_ctx *octx,
 		const out_val *sz,
 		unsigned align)
 {
-	/* TODO: align */
-	static int warn;
-	if(!warn){
-		fprintf(stderr, "TODO: align vla (%u)\n", align);
-		warn = 1;
-	}
-	return alloca_stack_adj(octx, op_minus, sz);
+	type *arith_ty = type_nav_btype(cc1_type_nav, type_intptr_t);
+	const out_val *adjusted_sp;
+
+	if(align < (unsigned)cc1_mstack_align)
+		align = cc1_mstack_align;
+
+	if(align == 1)
+		return alloca_stack_adj(octx, op_minus, sz);
+
+
+	/* add $16, %rsp */
+	sz = out_op(octx, op_plus, sz, out_new_l(octx, arith_ty, align));
+	adjusted_sp = alloca_stack_adj(octx, op_minus, sz);
+
+	/* and $~16, %rsp */
+	return out_op(octx, op_and,
+			adjusted_sp,
+			out_new_l(octx, arith_ty, ~((long)align - 1)));
 }
 
 void out_alloca_pop(out_ctx *octx, const out_val *sz)
