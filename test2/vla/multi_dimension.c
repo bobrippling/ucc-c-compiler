66bfc2373224e863e5150ee84be750ddfe4b4951
Rob Pilling <robpilling@gmail.com>
2014-07-12 12:46:35 +0100

    Add multi-dimensional vla test

--- test2/vla/multi_dimension.c
+++ test2/vla/multi_dimension.c
@@ -1,9 +1,11 @@
+// RUN: %ocheck 0 %s
+
 void abort(void);
 
 g() { return 3; }
 h() { return 2; }
 
-void init(int a, int b, int (*p)[a])
+void init(int a, int b, int (*p)[b])
 {
 	for(int i = 0; i < a; i++)
 		for(int j = 0; j < b; j++)
@@ -12,15 +14,20 @@ void init(int a, int b, int (*p)[a])
 
 f(int n)
 {
+	// sizeof(ar)  = 4 * (3 * 3) * 2 = 72
+	// sizeof(*ar) = 4           * 2 = 8
 	int ar[n * g()][h()];
 
-	if(sizeof ar != h() * g() * sizeof(int))
+	if(sizeof ar != h() * n * g() * sizeof(int))
 		abort();
 
 	if(sizeof *ar != h() * sizeof(int))
 		abort();
 
-	init(sizeof ar / sizeof *ar, sizeof *ar, ar);
+	if((int)(sizeof ar / sizeof *ar) != n * g())
+		abort();
+
+	init(sizeof ar / sizeof *ar, sizeof *ar / sizeof **ar, ar);
 
 	for(int i = 0; i < (int)(sizeof ar / sizeof *ar); i++)
 		for(int j = 0; j < (int)(sizeof *ar / sizeof **ar); j++)
