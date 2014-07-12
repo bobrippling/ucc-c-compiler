6ec3034e73953ff1437578773b48ba7ef7db132c
Rob Pilling <robpilling@gmail.com>
2014-07-12 12:46:29 +0100

    vm-init test at runtime

--- test2/vla/vm_init.c
+++ test2/vla/vm_init.c
@@ -1,9 +1,30 @@
-// RUN: %ucc -fsyntax-only %s
+// RUN: %ocheck 0 %s
 
-f(int n)
+syntax(int n)
 {
 	int ar[n];
 	int (*p)[n] = &ar;
 
-	g(p);
+	syntax(p);
+}
+
+assert(_Bool b)
+{
+	if(!b)
+		abort();
+}
+
+f(int n)
+{
+	short (*p)[n] = 0;
+
+	assert(p == 0);
+	assert((int)(p + 1) == 3 * sizeof(short));
+}
+
+main()
+{
+	f(3);
+
+	return 0;
 }
