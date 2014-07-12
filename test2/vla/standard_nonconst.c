49de1abd6d98f31d63c0be3df1b9395b8b79d09c
Rob Pilling <robpilling@gmail.com>
2014-07-12 12:46:44 +0100

    Complete and bolster vla/standard_nonconst.c test

--- test2/vla/standard_nonconst.c
+++ test2/vla/standard_nonconst.c
@@ -1,15 +1,26 @@
-// RUN: %ocheck 1 %s '-DNULL=(void *)0'
-// RUN: %ocheck 0 %s '-DNULL=0'
+// RUN: %ocheck 1 %s '-DNULL=(void *)0' -fno-fold-const-vlas
+// RUN: %ocheck 0 %s '-DNULL=0' -fno-fold-const-vlas
+// RUN: %check %s '-DNULL=(void *)0'
 
 int null_is_ptr_type()
 {
-	char s[1][1+(int)NULL];
+	char s[1][1+(int)NULL]; // CHECK: warning: cast-expr is a non-standard constant expression (for array size)
 	int i = 0;
-	sizeof s[i++];
+	sizeof(s[++i]);
+	return i;
+}
+
+int another()
+{
+	int i = 1;
+	sizeof(char[i++]);
 	return i;
 }
 
 main()
 {
+	if(another() != 2)
+		abort();
+
 	return null_is_ptr_type();
 }
