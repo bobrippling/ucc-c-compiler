4863e5c0c7749ceeeac5a7bd9533de9b801075ab
Rob Pilling <robpilling@gmail.com>
2014-08-05 23:29:41 +0100

    INT_MIN/MAX test fixes

--- test2/int_overflow/const_op.c
+++ test2/int_overflow/const_op.c
@@ -2,15 +2,16 @@
 
 enum
 {
-	INT_MAX = -1u / 2 - 1
+	INT_MAX = -1u / 2,
+	INT_MIN = -INT_MAX - 1,
 };
 
 // this is UB, but defined in ucc as 2's complement wrapping
-int under = -INT_MAX - 2;
-int over = INT_MAX + 1;
-
 main()
 {
+	int under = INT_MIN - 1;
+	int over = INT_MAX + 1;
+
 	if(under != 2147483647)
 		abort();
 	if(over != -2147483648)
--- test2/int_overflow/unsigned_to_signed.c
+++ test2/int_overflow/unsigned_to_signed.c
@@ -1,3 +1,3 @@
-// RUN: %check %s
+// RUN: %layout_check %s
 
-static const int INT_MAX = -1U / 2; // CHECK: warning: implicit cast changes value from 2147483648 to -2147483648
+const int INT_MAX = -1U / 2;
