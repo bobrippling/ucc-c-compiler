226d846c3d3b6b7b0bb056b5e6a1ced40886ab88
Rob Pilling <robpilling@gmail.com>
2014-08-14 22:13:38 +0100

    Example of stack-allocation TODO for inlining

---
+++ inline_alloc.c
@@ -0,0 +1,17 @@
+f(int x)
+{
+	int counts[] = { x-1, x, x+1 };
+
+	int t = 0;
+	for(int i = 0; i < sizeof(counts)/sizeof(counts[0]); i++)
+		t += counts[i];
+
+	return t;
+}
+
+main()
+{
+	int stash[] = { 1, 2 };
+
+	return stash[0] + f(3) + stash[1];
+}
