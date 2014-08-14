5950e0416659e3c86c162f9504f58c1ab70edf9b
Rob Pilling <robpilling@gmail.com>
2014-08-14 22:06:50 +0100

    Another inline recursion test

---
+++ test2/inline/recursive2.c
@@ -0,0 +1,22 @@
+// RUN: %ocheck 7 %s
+
+int count;
+
+int r(int x)
+{
+	if(x > 0){
+		count += x;
+		return r(x - 1);
+	}
+	return 7;
+}
+
+int main()
+{
+	int ret = r(3);
+
+	if(count != 6)
+		abort();
+
+	return ret;
+}
