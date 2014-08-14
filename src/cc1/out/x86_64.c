d7c2fa269e9147a36f4530ba4707cdb593147e72
Rob Pilling <robpilling@gmail.com>
2014-08-14 22:06:49 +0100

    Disable argument retain check on call gen

--- src/cc1/out/x86_64.c
+++ src/cc1/out/x86_64.c
@@ -1803,9 +1803,13 @@ const out_val *impl_call(
 				/* need to free it up, as v_to_reg_given doesn't clobber check */
 				v_freeup_reg(octx, rp);
 
+#if 0
+				/* argument retainedness doesn't matter here -
+				 * inline functions hold onto their arguments one extra */
 				UCC_ASSERT(local_args[i]->retains == 1,
 						"incorrectly retained arg %d: %d",
 						i, local_args[i]->retains);
+#endif
 
 
 				local_args[i] = v_to_reg_given(octx, local_args[i], rp);
