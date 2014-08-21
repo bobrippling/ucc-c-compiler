6e6e06992f89d29424b6b61928ca9d17540bb502
Rob Pilling <robpilling@gmail.com>
2014-08-21 21:50:53 +0100

    Inline infinite loop bug

---
+++ inline_bug2.c
@@ -0,0 +1,33 @@
+__attribute((always_inline))
+inteq(int a, int b)
+{
+	return a == b;
+}
+
+__attribute((always_inline))
+int *find(int *begin, int *end, int n, int cmp(int, int))
+{
+	int *i;
+	for(i = begin; i != end; i++)
+		if(cmp(*begin, n))
+			break;
+	return begin;
+}
+
+int main()
+{
+	int ar[] = {
+		1, 2, 3, 4, 5
+	};
+	int *arend = ar + 5;
+
+	int *four = find(ar, arend, 4, inteq);
+
+	if(four != &ar[3])
+		printf(":(\n");
+
+	int *end = find(ar, arend, 0, inteq);
+
+	if(end != &ar[5])
+		printf(":(\n");
+}
