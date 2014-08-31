4511770493bd708c7ec53072d6c49e2d237e8c55
Rob Pilling <robpilling@gmail.com>
2014-09-01 00:41:37 +0100

    Bugs with esoteric examples

---
+++ time.c
@@ -0,0 +1,8 @@
+// final newline isn't emitted correctly
+// ^ old, current bug is a spill bug, likely fixed in the bootstrap branch
+
+main(_)
+{
+	_^448&&main(-~_);
+	putchar(--_%64?32|-~7[__TIME__-_/8%8][">'txiZ^(~z?"-48]>>";;;====~$::199"[_*2&8|_/64]/(_&2?1:8)%8&1:10);
+}
