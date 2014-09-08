3c7f97688b0c3bb6fbaff2a2efe541f50dc7373b
Rob Pilling <robpilling@gmail.com>
2014-09-08 21:58:46 +0100

    Make callee_save_use.c bug more obvious

--- callee_save_use.c
+++ callee_save_use.c
@@ -1,6 +1,21 @@
-main()
+use_ebx()
 {
 	int ebx_temp;
+	// having "m" here causes the bug, as the asm() temp spill conflicts
 	__asm("mov %1, %0" : "=b"(ebx_temp)  : "m"(3));
 	return ebx_temp;
+}
+
+main()
+{
+	__asm("mov %0, %%ebx" : : "g"(72));
+
+	if(use_ebx() != 3)
+		abort();
+
+	int ebx_after;
+	__asm("mov %%ebx, %0" : "=g"(ebx_after));
+
+	if(ebx_after != 72)
+		abort();
 }
