7a020ff7217cfe599f86f5ea55a957f9e10173cd
Rob Pilling <robpilling@gmail.com>
2014-09-14 22:57:31 +0100

    Move alignof() test into test2/

--- alignof_crash.c
+++
@@ -1,8 +0,0 @@
-//_Alignas(__alignof__(struct sockaddr *))
-__attribute__((aligned((__alignof__(struct sockaddr *)))))
-
-struct A
-{
-	unsigned short f;
-	char data[128 - sizeof(unsigned short)];
-};
