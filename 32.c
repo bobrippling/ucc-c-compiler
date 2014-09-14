38896c6957f6a1fc3c3a0de7d87e4f5b0b7a36b8
Rob Pilling <robpilling@gmail.com>
2014-09-14 13:30:50 +0100

    VLA ice

---
+++ 32.c
@@ -0,0 +1,31 @@
+g()
+{
+	return 5;
+}
+
+populate(int *p, int n)
+{
+	while(n --> 0)
+		*p++ = n;
+}
+
+sum(int *p, int n)
+{
+	int t = 0;
+	while(n --> 0)
+		t += *p++;
+	return t;
+}
+
+f(int n)
+{
+	int x[n + g()];
+
+	populate(x, sizeof x / sizeof *x);
+	return sum(x, sizeof x / sizeof *x);
+}
+
+main()
+{
+	printf("hi %d\n", f(3));
+}
