53bdc90806233120b18fc1b8c4c9340ecbc5ff62
Rob Pilling <robpilling@gmail.com>
2014-10-05 12:17:59 +0100

    Test for leaks/errors in inline-local-sym-overwriting

---
+++ test2/inline/sym_overwrite.c
@@ -0,0 +1,23 @@
+// RUN: %ucc -o %t %s
+
+int printf(const char *, ...)
+	__attribute((format(printf, 1, 2)));
+
+void f(int n)
+{
+	int b = 0; // b sym pushed/popped on inline
+	printf("new b @ %p\n", &b);
+	vla(n);
+}
+
+void vla(int n)
+{
+	typedef int ar[n]; // ar sym pushed/popped on inline
+	printf("new vla, sizeof = %d\n", sizeof(ar));
+	f(n);
+}
+
+main()
+{
+	vla(2);
+}
