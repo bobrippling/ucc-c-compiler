6fed18f0899b2709f6937f32a2778ac5a0227795
Rob Pilling <robpilling@gmail.com>
2014-10-05 12:13:06 +0100

    struct_arg.c - updated

--- struct_arg.c
+++ struct_arg.c
@@ -1,14 +1,27 @@
 struct A
 {
 	int i, j;
+	long l;
 };
 
-f(struct A a)
+void f(struct A a, struct A b)
+#ifdef IMPL
 {
-	return a.i + a.j;
-}
+	int printf(const char *, ...);
 
-main()
-{
-	f((struct A){ 1, 2 });
+	printf("%d %d %ld, %d %d %ld\n",
+			a.i, a.j, a.l,
+			b.i, b.j, b.l);
 }
+#else
+	;
+#endif
+
+#ifndef IMPL
+int main()
+{
+	struct A b = { 1, 2, 3 };
+
+	f((struct A){ 5, 2, 6 }, b);
+}
+#endif
