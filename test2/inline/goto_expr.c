cb106ec75005f84c33f8135951b6203b2659625c
Rob Pilling <robpilling@gmail.com>
2014-11-03 23:26:44 +0000

    goto *expr/label test

---
+++ test2/inline/goto_expr.c
@@ -0,0 +1,35 @@
+// RUN: %ocheck 0 %s
+
+__attribute((always_inline))
+int f(int x)
+{
+	void *jmps[] = {
+		&&a, &&b, &&c
+	};
+
+	goto *jmps[x];
+b:
+	return x + 3;
+c:
+	return x + 1;
+a:
+	x += 2;
+	goto *&&b;
+}
+
+assert(int c)
+{
+	if(!c){
+		extern void abort(void);
+		abort();
+	}
+}
+
+main()
+{
+	assert(f(0) == 5);
+	assert(f(1) == 4);
+	assert(f(2) == 3);
+
+	return 0;
+}
