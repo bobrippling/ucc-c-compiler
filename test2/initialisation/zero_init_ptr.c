ec2c28cdd42d2153c37f5fd441813f7f0d6db16f
Rob Pilling <robpilling@gmail.com>
2014-12-09 23:04:09 +0000

    Tests for initialisers set within their own init

---
+++ test2/initialisation/zero_init_ptr.c
@@ -0,0 +1,34 @@
+struct A
+{
+	int before;
+	const char *p;
+	int after;
+};
+
+const char *f(int *pb, int *pa)
+{
+	*pb = 5;
+	*pa = 8;
+	return "hi";
+}
+
+int strcmp(const char *, const char *);
+_Noreturn void abort(void);
+
+main()
+{
+	struct A a = {
+		.p = f(&a.before, &a.after)
+	};
+
+	if(strcmp(a.p, "hi"))
+		abort();
+
+	if(a.after != 0)
+		abort();
+
+	if(a.before != 5)
+		abort();
+
+	return 0;
+}
