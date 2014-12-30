34a35dc30013b14298c1d0def5a1944f356ab022
Rob Pilling <robpilling@gmail.com>
2014-12-30 01:05:56 +0000

    Fix common-symbols tests for targets with leading-underscores

--- test2/initialisation/comm_align.c
+++ test2/initialisation/comm_align.c
@@ -1,4 +1,4 @@
-// RUN: %ucc -S -o- %s | grep -F .comm | %output_check '.comm i,1,8' '.comm j,1,4' '.comm k,1,2'
+// RUN: %ucc -fno-leading-underscore -S -o- %s | grep -F .comm | %output_check '.comm i,1,8' '.comm j,1,4' '.comm k,1,2'
 
 char i __attribute((aligned(8)));
 _Alignas(int) char j;
