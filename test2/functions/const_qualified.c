e16bb5fdd291912312a7dbd064f1854133723a8e
Rob Pilling <robpilling@gmail.com>
2015-01-18 23:39:24 +0000

    Update qualification test - check _Atomic qualification

--- test2/functions/const_qualified.c
+++ test2/functions/const_qualified.c
@@ -2,3 +2,5 @@
 
 typedef int fn(void);
 const fn x; // CHECK: warning: qualifier on function type 'fn (aka 'int (void)')'
+
+_Atomic fn y; // CHECK: warning: qualifier on function type 'fn (aka 'int (void)')'
