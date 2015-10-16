e5ebab9059a02ae3ce45ef40bd86c5f46a2676b5
Rob Pilling <robpilling@gmail.com>
2015-10-16 21:52:36 +0100

    Pull out warning code to warn.c

--- src/cc1/warn.h
+++ src/cc1/warn.h
@@ -1,6 +1,19 @@
 #ifndef CC1_WARN_H
 #define CC1_WARN_H
 
+#include "../util/dynmap.h"
+
+enum warning_fatality
+{
+	W_OFF = 0,
+	W_WARN = 1,
+	W_ERROR = 2, /* set by -Werror */
+
+	W_NO_ERROR = 3
+	/* set by -Wno-error=xyz
+	 * not reset to W_WARN since -Werror/-Wno-error will alter this */
+};
+
 struct cc1_warning
 {
 #define X(flag, name) \
@@ -27,5 +40,19 @@ int cc1_warn_at_w(
 
 #define cc1_warn_at(loc, warn, ...) \
 		cc1_warn_at_w(loc, &cc1_warning.warn, __VA_ARGS__)
+
+void warning_init(void);
+
+void warnings_set(enum warning_fatality to);
+
+void warning_pedantic(enum warning_fatality set);
+
+void warning_on(
+		const char *warn, enum warning_fatality to,
+		int *const werror, dynmap *unknowns);
+
+void warnings_upgrade(void);
+
+int warnings_check_unknown(dynmap *unknown_warnings);
 
 #endif
