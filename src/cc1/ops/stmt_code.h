48f5eea32c58887af8ab7ed144cfc235e70c137d
Rob Pilling <robpilling@gmail.com>
2015-12-15 21:48:37 +0000

    Reorganise stmt_code.h

--- src/cc1/ops/stmt_code.h
+++ src/cc1/ops/stmt_code.h
@@ -4,12 +4,19 @@ STMT_DEFS(code);
 
 struct out_dbg_lbl;
 
+
+/* folding */
+void fold_check_scope_entry(where *, const char *desc,
+		symtable *const s_from, symtable *const s_to);
+
+void fold_shadow_dup_check_block_decls(symtable *stab);
+
+
+/* native code-gen */
 void gen_block_decls(
 		symtable *,
 		struct out_dbg_lbl *[ucc_static_param 2],
 		out_ctx *);
-
-void gen_ir_block_decls(symtable *symtab, struct irctx *ctx);
 
 void gen_block_decls_dealloca(
 		symtable *stab,
@@ -18,11 +25,6 @@ void gen_block_decls_dealloca(
 
 void gen_scope_leave(symtable *s_from, symtable *s_to, out_ctx *octx);
 void gen_scope_leave_parent(symtable *s_from, out_ctx *octx);
-
-void fold_check_scope_entry(where *, const char *desc,
-		symtable *const s_from, symtable *const s_to);
-
-void fold_shadow_dup_check_block_decls(symtable *stab);
 
 void gen_stmt_code_m1(
 		const stmt *s,
@@ -34,5 +36,9 @@ void gen_stmt_code_m1_finish(
 		const stmt *s,
 		struct out_dbg_lbl *pushed_lbls[ucc_static_param 2],
 		out_ctx *octx);
+
+
+/* ir code-gen */
+void gen_ir_block_decls(symtable *symtab, struct irctx *ctx);
 
 void gen_ir_stmt_code_m1(const stmt *s, struct irctx *ctx, const int m1);
