bd305330a0fe855e5fe4e207c06ecd4ea1c7b943
Rob Pilling <robpilling@gmail.com>
2016-02-24 21:39:55 +0000

    Editing union.c

--- union.c
+++ union.c
@@ -1,18 +1,28 @@
 union
 {
-	int x;
+	int i;
+	struct
+	{
+		unsigned a : 3;
+		unsigned b : 4;
+	} bfs;
+	struct
+	{
+		char c; // TODO: access this
+	};
 	struct
 	{
 		char c;
-	};
+	} named;
 	long long l;
 	struct
 	{
-		int a, b, c;
+		int x, y, z;
 	} z;
-} u;
+} u = { .bfs = { 2, 3 } };
 
 f()
 {
-	return u.z.b;
+	u.c = 7;
+	return u.z.y;
 }
