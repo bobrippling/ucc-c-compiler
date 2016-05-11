68d4ef2636f1a6a2c714515a9d85363af300ad80
Rob Pilling <robpilling@gmail.com>
2016-05-11 22:44:52 +0100

    Change debug scope checks to use debug-scope / stdoutcheck

--- test/cases/debug/line_scope.c
+++ test/cases/debug/line_scope.c
@@ -1,17 +1,21 @@
-// RUN: %debug_scope %s
+// RUN: %debug_scope %s | %stdoutcheck %s
 
 int printf(const char *, ...) __attribute__((format(printf, 1, 2)));
 
-int f(int arg) // SCOPE: arg ret
-{ // SCOPE: arg ret
-	int ret = 5 + arg; // SCOPE: arg ret
-	for(int i = 0; i < 3; i++){ // SCOPE: arg ret i
-		printf("%d\n", i); // SCOPE: arg ret i
-	} // SCOPE: arg ret
-	return ret; // SCOPE: arg ret
-} // SCOPE: arg
+int f(int arg)
+{
+	int ret = 5 + arg;
+	for(int i = 0; i < 3; i++){
+		printf("%d\n", i);
+	}
+	return ret;
+}
 
 main()
 {
 	return f(2);
 }
+
+// STDOUT: arg 3 12
+// STDOUT: i 5 10
+// STDOUT: ret 3 12
