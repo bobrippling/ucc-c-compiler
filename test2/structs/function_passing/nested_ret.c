bdaf2d1c7d29504c6ef04a0c130e7300ed8af5e9
Rob Pilling <robpilling@gmail.com>
2016-07-20 23:10:37 +0100

    Add stret ABI-bug for nested structs

---
+++ test2/structs/function_passing/nested_ret.c
@@ -0,0 +1,28 @@
+// RUN: %ocheck 0 %s
+
+struct A
+{
+	struct B
+	{
+		float a;
+	} b;
+	double x;
+};
+
+struct A f(void)
+{
+	return (struct A){ 1, 2 };
+}
+
+int main()
+{
+	// this currently works, but doesn't match the platform ABI
+	struct A a = f();
+
+	if(a.b.a != 1)
+		abort();
+	if(a.x != 2)
+		abort();
+
+	return 0;
+}
