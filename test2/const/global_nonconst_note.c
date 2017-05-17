fe4ab7f329a38c54a64903090aa7824a7e6b5814
Rob Pilling <robpilling@gmail.com>
2017-05-17 19:09:25 +0100

    Fix note string in test

--- test2/const/global_nonconst_note.c
+++ test2/const/global_nonconst_note.c
@@ -3,4 +3,4 @@
 int f(int);
 
 int i = 1 + f(3) * 2; // CHECK: error: global scalar initialiser not constant
-// CHECK: ^ note: first non-constant expression here (funcall)
+// CHECK: ^ note: first non-constant expression here (function-call)
