f857cf1d2bc5b22104563cd3fe0ef4addc9f994e
Rob Pilling <robpilling@gmail.com>
2017-05-17 19:11:33 +0100

    fix parse error in offsetof() test

--- test2/builtin/offsetof_bad.c
+++ test2/builtin/offsetof_bad.c
@@ -2,4 +2,4 @@
 
 struct A;
 
-a = __builtin_offsetof(struct A, a)]; // CHECK: error: offsetof() on incomplete type 'struct A'
+a = __builtin_offsetof(struct A, a); // CHECK: error: offsetof() on incomplete type 'struct A'
