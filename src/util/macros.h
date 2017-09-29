d1fd8f04754741ae9494bd3ef95ea0ca84472369
Rob Pilling <robpilling@gmail.com>
2017-09-29 19:28:28 +0100

    util header guards redefining countof()

--- src/util/macros.h
+++ src/util/macros.h
@@ -1,6 +1,8 @@
 #ifndef UTIL_MACROS_H
 #define UTIL_MACROS_H
 
-#define countof(ar) (sizeof(ar) / sizeof((ar)[0]))
+#ifndef countof
+#  define countof(ar) (sizeof(ar) / sizeof((ar)[0]))
+#endif
 
 #endif
