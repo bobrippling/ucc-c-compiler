7e7c18192715f6c8063dce6d7c3a0da905a6bd30
Rob Pilling <robpilling@gmail.com>
2018-06-04 21:47:18 +0100

    Update tests to handle new indication of sysheader

--- test2/warnings/wsysheader.c
+++ test2/warnings/wsysheader.c
@@ -1,12 +1,12 @@
-// RUN: %check --prefix=no_flags %s -x cpp-output -Wimplicit
+// RUN: %check --prefix=no-flags %s -x cpp-output -Wimplicit
 // RUN: %check --prefix=wnosysh  %s -x cpp-output -Wimplicit -Wno-system-headers
 // RUN: %check --prefix=wsysh    %s -x cpp-output -Wimplicit -Wsystem-headers
 
 // flag "3" means system header:
 # 7 "stdio.h" 1 3
-// ^ the above 5 is volatile to change
+// ^ the above 7 is volatile to change
 
-f(); // CHECK-no_flags: !/warning/
+f(); // CHECK-no-flags: !/warning/
 // CHECK-wnosysh: ^ !/warning/
 // CHECK-wsysh: ^^ /warning/
 
