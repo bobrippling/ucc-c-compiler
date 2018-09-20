7b610d6466addc008ce28fe8a346544c0547c723
Rob Pilling <robpilling@gmail.com>
2018-09-20 18:30:51 +0100

    Fix cpp/has_inc.c test - don't rely on system includes

--- test2/cpp/has_inc.c
+++ test2/cpp/has_inc.c
@@ -1,20 +1,23 @@
-// RUN: %ucc -E -o %t %s
+// RUN: %ucc -E -o %t %s -nostdinc -isystem cpp/inc/
 // RUN: ! grep '^no' %t
 
 #if !defined(__has_include)
 #  error no __has_include
 #endif
 
-#if defined(__has_include) && __has_include(<stdlib.h>)
-got stdlib (complex expr)
+/* Include files reference has/ under this file's
+ * directory, so we're not dependant on the
+ * environment for tests. */
+#if defined(__has_include) && __has_include(<stdio.h>)
+got stdio (complex expr)
 #else
-no stdlib (complex expr)
+no stdio (complex expr)
 #endif
 
 #if __has_include(<stdio.h>)
-got stdio
+got stdio (normal expr)
 #else
-no stdio
+no stdio (normal expr)
 #endif
 
 #define STDIO <stdio.h>
