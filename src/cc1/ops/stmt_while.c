2d590888d383f1a22a68bdc1e5ada9573c27bb5b
Rob Pilling <robpilling@gmail.com>
2018-09-20 21:11:15 +0100

    Explicitly state block creation order for consistent labels in code-gen
    ... rather than having a non-strictly conforming program which varies
    based on the host compiler. There's no problem with code-gen, just the
    numbering of blocks generated for while statements.
    
    Clearly if ucc was self-hosting, this wouldn't be a problem, but it
    isn't, yet.

--- src/cc1/ops/stmt_while.c
+++ src/cc1/ops/stmt_while.c
@@ -27,9 +27,11 @@ void gen_stmt_while(const stmt *s, out_ctx *octx)
 	struct out_dbg_lbl *endlbls[2][2];
 	out_blk *blk_body = out_blk_new(octx, "while_body");
 
-	stmt_init_blks(s,
-			out_blk_new(octx, "while_cont"),
-			out_blk_new(octx, "while_break"));
+	{
+		out_blk *blk_cont = out_blk_new(octx, "while_cont");
+		out_blk *blk_break = out_blk_new(octx, "while_break");
+		stmt_init_blks(s, blk_cont, blk_break);
+	}
 
 	out_ctrl_transfer(octx, s->blk_continue, NULL, NULL);
 
