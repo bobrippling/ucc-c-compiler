d5f7b01cab5f7448b0668e3b1b58b8ff20abf5fe
Rob Pilling <robpilling@gmail.com>
2019-01-03 22:31:34 +0000

    Emit debug-end labels immediately
    This gives more fine-grained debug-scope information.
    
    The labels must be kept on the blocks (or on pending_lbls) in case
    we're in dead-code - we still want the labels to exist for the debug
    code to reference.

--- src/cc1/out/dbg_lbl.c
+++ src/cc1/out/dbg_lbl.c
@@ -76,6 +76,10 @@ void out_dbg_label_pop(out_ctx *octx, struct out_dbg_lbl *to_pop)
 
 	/* this is the block we should emit the label from */
 	add_lbl_to_blk(to_pop, &blk->labels.end);
+	if(!to_pop->emitted){
+		out_asm_label(octx, to_pop->lbl);
+		to_pop->emitted = 1;
+	}
 
 	/* release the caller's hold / hold returned by out_dbg_label_push() */
 	RELEASE(to_pop);
