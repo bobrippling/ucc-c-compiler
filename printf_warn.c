aa52880a0318d5d52f7ddab2f55a7cf4daa931ff
Rob Pilling <robpilling@gmail.com>
2019-02-04 20:32:56 +0000

    Additional test case

--- printf_warn.c
+++ printf_warn.c
@@ -3,4 +3,6 @@ main()
 	int printf(const char *, ...) __attribute((format(printf, 1, 2)));
 
 	printf("%*.*s\n", 5, 3, "abcdefghijklmno");
+
+	printf("hi %d\n", "yo"); // CHECK: warning: format %d expects integral argument (got char *)
 }
