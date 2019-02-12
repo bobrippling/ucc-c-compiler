67d04cb7ad26ce79a0ea3cf609ab06fa9755b305
Rob Pilling <robpilling@gmail.com>
2019-02-12 20:36:14 +0000

    Remove unnecessary error check on __asm__/escaped.c test

--- test/__asm__/escaped.c
+++ test/__asm__/escaped.c
@@ -1,4 +1,6 @@
 // RUN: %ucc -S -o- %s
-// RUN: %check -e %s
+
+// may error, may not - depends on the assembler
+
 x __asm("0new\nli\rne"); // CHECK: /warning: asm name contains character 0xa/
 __asm("hi\nthere");
