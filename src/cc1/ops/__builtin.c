3b5207563e63d95fc6a4cddda65b2727c0872ff7
Rob Pilling <robpilling@gmail.com>
2019-03-06 23:25:16 +0000

    Allow NAN to be a constant expression

--- src/cc1/ops/__builtin.c
+++ src/cc1/ops/__builtin.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <math.h>
 
 #include "../../util/util.h"
 #include "../../util/dynarray.h"
@@ -850,6 +851,19 @@ need_char_p:
 	wur_builtin(e);
 }
 
+static void const_nan(expr *e, consty *k)
+{
+	CONST_FOLD_LEAF(k);
+	k->type = CONST_NUM;
+	k->bits.num.val.f = NAN;
+
+	switch(e->bits.builtin_nantype){
+		case builtin_nanf: k->bits.num.suffix = VAL_FLOAT; break;
+		case builtin_nan:  k->bits.num.suffix = VAL_DOUBLE; break;
+		case builtin_nanl: k->bits.num.suffix = VAL_LDOUBLE; break;
+	}
+}
+
 static const out_val *builtin_gen_nan(const expr *e, out_ctx *octx)
 {
 	return out_new_nan(octx, e->tree_type);
@@ -858,6 +872,7 @@ static const out_val *builtin_gen_nan(const expr *e, out_ctx *octx)
 static expr *builtin_nan_mutate(expr *e)
 {
 	expr_mutate_builtin(e, nan);
+	e->f_const_fold = const_nan;
 	return e;
 }
 
