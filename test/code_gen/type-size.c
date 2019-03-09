de434ac65a4d559d7e5beb0bd1c6026ba0ad9b5c
Rob Pilling <robpilling@gmail.com>
2019-03-09 12:32:48 +0000

    Update .type/.size tests

--- test/code_gen/type-size.c
+++ test/code_gen/type-size.c
@@ -2,7 +2,7 @@
 // RUN: grep -F '.type f,@function' %t
 // RUN: grep -F '.size f, .-f' %t
 // RUN: grep -F '.type i,@object' %t
-// RUN: grep -F '.size i, .-i' %t
+// RUN: grep -F '.size i, 4' %t
 
 int i = 3;
 
