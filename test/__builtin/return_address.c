0df07367e120ddc804bee300c1090f221b983726
Rob Pilling <robpilling@gmail.com>
2019-03-09 10:41:03 +0000

    Fix __builtin_return_address() test for leading-underscore targets

--- test/__builtin/return_address.c
+++ test/__builtin/return_address.c
@@ -1,15 +1,18 @@
 // RUN: %ocheck 0 %s
 
-#define ASM_LABEL(name)  \
-__asm(#name ":"); \
+#define ASM_DECL(name) \
 extern char name[] __asm(#name)
+
+#define ASM_LABEL(name) \
+__asm(#name ":"); \
+ASM_DECL(name)
 
 void *call_location(void)
 {
 	return __builtin_extract_return_addr(__builtin_return_address(0));
 }
 
-extern char after_main[];
+ASM_DECL(after_main);
 
 ASM_LABEL(before_main);
 int main()
