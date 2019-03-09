3f43166952c7a79a4be573bc91178256f8553f6b
Rob Pilling <robpilling@gmail.com>
2019-03-09 11:16:28 +0000

    Fix lib's missing config_as.h - use defs.h

--- lib/arch/x86_64/defs.h
+++ lib/arch/x86_64/defs.h
@@ -2,3 +2,13 @@
 #define JOIN(a, b) JOIN_(a, b)
 
 #define SYMBL(x) JOIN(__USER_LABEL_PREFIX__, x)
+
+#if defined(__linux__)
+#  define SECTION_NAME_TEXT .text
+#  define SECTION_NAME_BSS .bss
+#elif defined(__DARWIN__)
+#  define SECTION_NAME_TEXT __TEXT,__text
+#  define SECTION_NAME_BSS __BSS,__bss
+#else
+#  error unknown target
+#endif
