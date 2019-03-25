5ffcc6ae21937e80848d6e37b23734aa999ab6af
Rob Pilling <robpilling@gmail.com>
2019-03-25 18:49:07 +0000

    Fix filesmodes multiple-inc and backslash-eof

--- src/ucc/filemodes.h
+++ src/ucc/filemodes.h
@@ -1,7 +1,12 @@
+#ifndef FILEMODES_H
+#define FILEMODES_H
+
 #define FILEMODES \
 	X(preproc, "c", 'i') \
 	X(compile, "cpp-output", 's') \
 	X(assemb, "assembler", 'o') \
 	ALIAS(assemb, "asm") \
 	X(assemb_with_cpp, "assembler-with-cpp", 'o') \
-	ALIAS(assemb_with_cpp, "asm-with-cpp") \
+	ALIAS(assemb_with_cpp, "asm-with-cpp")
+
+#endif
