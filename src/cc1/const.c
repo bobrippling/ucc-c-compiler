32f57f472883afb78221e4a733019ba17ed58194
Rob Pilling <robpilling@gmail.com>
2019-03-29 18:07:44 +0000

    Handle alternative ways of generating NaN

--- src/cc1/const.c
+++ src/cc1/const.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <math.h>
 #include <assert.h>
 
 #include "../util/util.h"
@@ -278,8 +279,19 @@ floating_t const_op_exec_fp(
 			/* explicitly bad */
 			ICE("floating point %s", op_to_str(op));
 
+		case op_divide:
+			if(lv == 0 && *rv == 0){
+				/* Special case 0/0, as this is a common way of defining NaN in libcs
+				 * when GNU __builtin_nan support isn't detected.
+				 *
+				 * We explicitly return NAN here as this isn't affected by the rounding
+				 * mode, whereas performing the calculation could return -nan.
+				 */
+				return NAN;
+			}
+			return lv / *rv; /* safe - / 0.0f is inf */
+
 		case op_multiply: return lv * *rv;
-		case op_divide:   return lv / *rv; /* safe - / 0.0f is inf */
 		case op_plus:     return rv ? lv + *rv : +lv;
 		case op_minus:    return rv ? lv - *rv : -lv;
 		case op_eq:       return lv == *rv;
