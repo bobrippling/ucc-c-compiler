3c58a137cf70e4b176e8d14ff835c24c2f7c14c1
Rob Pilling <robpilling@gmail.com>
2019-04-04 18:13:13 +0100

    Add __builtin_speculation_safe_value

--- spectre.c
+++ spectre.c
@@ -7,3 +7,11 @@ void mem(void (**p)())
 {
 	(*p)(1,2,3);
 }
+
+int f(int *p, int i)
+{
+#ifndef __HAVE_BUILTIN_SPECULATION_SAFE_VALUE
+#error not defined
+#endif
+	return p[__builtin_speculation_safe_value(i /*, failvalue*/)];
+}
