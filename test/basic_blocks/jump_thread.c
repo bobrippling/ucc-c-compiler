66d64052acf72266e2c5b26c39c470f8561dfddb
Rob Pilling <robpilling@gmail.com>
2019-04-12 23:38:07 +0100

    Additional jump tests

--- test/basic_blocks/jump_thread.c
+++ test/basic_blocks/jump_thread.c
@@ -1,6 +1,6 @@
 // RUN: %jmpcheck %s
 
-f(int cond)
+void gotos(int cond)
 {
 	goto b;
 a:
@@ -9,4 +9,44 @@ b:
 	goto a;
 c:
 	goto b;
+}
+
+void infinite_loop_simple(int cond)
+{
+a:
+	goto a;
+}
+
+void infinite_loop_complex()
+{
+	goto a;
+c: goto a;
+b: goto c;
+a: goto b;
+}
+
+void infinite_loop_complex_straightline()
+{
+a:
+b:
+c:
+	goto d;
+d:
+	goto e;
+e:
+	goto a;
+}
+
+void infinite_loop_with_code()
+{
+	if(g())
+		a();
+	else
+		b();
+
+	goto a;
+
+c: goto a;
+b: goto c;
+a: goto b;
 }
