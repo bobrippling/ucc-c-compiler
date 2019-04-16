b2bfdc4d5fa107dfcb81a2523f988e51c723291a
Rob Pilling <robpilling@gmail.com>
2019-04-16 20:40:24 +0100

    Move -fuse-cpp=... to CFLAGS_BOOTSTRAP

--- Bootstrap.mk
+++ Bootstrap.mk
@@ -3,6 +3,7 @@ CONFIGURE_OUTPUT = src/config.custom.mk
 
 # -Wno-return-undef: reachability depends on libc's _Noreturn/__GNUC__
 CFLAGS_BOOTSTRAP = \
+		 -fuse-cpp=${PWD}/tools/syscpp \
 		 -fshow-warning-option \
 		 -Wno-return-undef
 
@@ -24,13 +25,13 @@ stage1: src
 
 bootstrap/stage2/${CONFIGURE_OUTPUT}: tools/link_r
 	mkdir -p bootstrap/stage2
-	cd bootstrap/stage2 && ../../configure CC=${PWD}/src/ucc/ucc\ -fuse-cpp=${PWD}/tools/syscpp 'CFLAGS=${CFLAGS_BOOTSTRAP}'
+	cd bootstrap/stage2 && ../../configure CC=${PWD}/src/ucc/ucc 'CFLAGS=${CFLAGS_BOOTSTRAP}'
 stage2: stage1 bootstrap/stage2/${CONFIGURE_OUTPUT}
 	make -Cbootstrap/stage2/src
 
 bootstrap/stage3/${CONFIGURE_OUTPUT}:
 	mkdir -p bootstrap/stage3
-	cd bootstrap/stage3 && ../../configure CC=${PWD}/bootstrap/stage2/src/ucc/ucc\ -fuse-cpp=${PWD}/tools/syscpp 'CFLAGS=${CFLAGS_BOOTSTRAP}'
+	cd bootstrap/stage3 && ../../configure CC=${PWD}/bootstrap/stage2/src/ucc/ucc 'CFLAGS=${CFLAGS_BOOTSTRAP}'
 stage3: stage2 bootstrap/stage3/${CONFIGURE_OUTPUT}
 	make -Cbootstrap/stage3/src
 
