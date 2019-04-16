9e4ee41cb845b40d4fc27c60d1f1c123e37f2d08
Rob Pilling <robpilling@gmail.com>
2019-04-16 19:46:28 +0100

    Bootstrap warnings: Disable -Wreturn-undef for bootstrap builds

--- Bootstrap.mk
+++ Bootstrap.mk
@@ -1,6 +1,11 @@
 PWD = $(shell pwd)
 CONFIGURE_OUTPUT = src/config.custom.mk
 
+# -Wno-return-undef: reachability depends on libc's _Noreturn/__GNUC__
+CFLAGS_BOOTSTRAP = \
+		 -fshow-warning-option \
+		 -Wno-return-undef
+
 .PHONY: bootstrap clean-bootstrap clean-stage1 clean-stage2 clean-stage3
 
 bootstrap: stage3
@@ -19,13 +24,13 @@ stage1: src
 
 bootstrap/stage2/${CONFIGURE_OUTPUT}: tools/link_r
 	mkdir -p bootstrap/stage2
-	cd bootstrap/stage2 && ../../configure CC=${PWD}/src/ucc/ucc\ -fuse-cpp=${PWD}/tools/syscpp
+	cd bootstrap/stage2 && ../../configure CC=${PWD}/src/ucc/ucc\ -fuse-cpp=${PWD}/tools/syscpp 'CFLAGS=${CFLAGS_BOOTSTRAP}'
 stage2: stage1 bootstrap/stage2/${CONFIGURE_OUTPUT}
 	make -Cbootstrap/stage2/src
 
 bootstrap/stage3/${CONFIGURE_OUTPUT}:
 	mkdir -p bootstrap/stage3
-	cd bootstrap/stage3 && ../../configure CC=${PWD}/bootstrap/stage2/src/ucc/ucc\ -fuse-cpp=${PWD}/tools/syscpp
+	cd bootstrap/stage3 && ../../configure CC=${PWD}/bootstrap/stage2/src/ucc/ucc\ -fuse-cpp=${PWD}/tools/syscpp 'CFLAGS=${CFLAGS_BOOTSTRAP}'
 stage3: stage2 bootstrap/stage3/${CONFIGURE_OUTPUT}
 	make -Cbootstrap/stage3/src
 
