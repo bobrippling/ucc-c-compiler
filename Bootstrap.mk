acfc3458cfc1ce5bcf521d063525a290b88361d4
Rob Pilling <robpilling@gmail.com>
2019-04-19 08:47:45 +0100

    Factor our CC_STAGE* paths

--- Bootstrap.mk
+++ Bootstrap.mk
@@ -7,6 +7,10 @@ CFLAGS_BOOTSTRAP = \
 		 -fshow-warning-option \
 		 -Wno-return-undef
 
+CC_STAGE1 = ${PWD}/src/ucc/ucc
+CC_STAGE2 = ${PWD}/bootstrap/stage2/src/ucc/ucc
+CC_STAGE3 = ${PWD}/bootstrap/stage3/src/ucc/ucc
+
 .PHONY: bootstrap clean-bootstrap clean-stage1 clean-stage2 clean-stage3
 
 bootstrap: stage3
@@ -25,13 +29,13 @@ stage1: src
 
 bootstrap/stage2/${CONFIGURE_OUTPUT}: tools/link_r
 	mkdir -p bootstrap/stage2
-	cd bootstrap/stage2 && ../../configure CC=${PWD}/src/ucc/ucc 'CFLAGS=${CFLAGS_BOOTSTRAP}'
+	cd bootstrap/stage2 && ../../configure CC=${CC_STAGE1} 'CFLAGS=${CFLAGS_BOOTSTRAP}'
 stage2: stage1 bootstrap/stage2/${CONFIGURE_OUTPUT}
 	make -Cbootstrap/stage2/src
 
 bootstrap/stage3/${CONFIGURE_OUTPUT}:
 	mkdir -p bootstrap/stage3
-	cd bootstrap/stage3 && ../../configure CC=${PWD}/bootstrap/stage2/src/ucc/ucc 'CFLAGS=${CFLAGS_BOOTSTRAP}'
+	cd bootstrap/stage3 && ../../configure CC=${CC_STAGE2} 'CFLAGS=${CFLAGS_BOOTSTRAP}'
 stage3: stage2 bootstrap/stage3/${CONFIGURE_OUTPUT}
 	make -Cbootstrap/stage3/src
 
@@ -39,4 +43,4 @@ tools/link_r: tools/link_r.c
 	${CC} -o $@ $<
 
 check-bootstrap: bootstrap
-	cd test; ./run_tests -i ignores --ucc=../bootstrap/stage3/src/ucc/ucc .
+	cd test; ./run_tests -i ignores '--ucc=${CC_STAGE3}' .
