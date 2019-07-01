46d3d8eca8fb191c4f0d70e03deb2c2b33b6d222
Rob Pilling <robpilling@gmail.com>
2019-07-01 18:47:48 +0100

    Add 64-bit argument test

--- test/literals/64_bit.c
+++ test/literals/64_bit.c
@@ -6,10 +6,14 @@ typedef long long ll;
 
 ll mem;
 
+void f(ll);
+
 main()
 {
 	ll stk = VAL; // assignment to stack
 	mem = VAL; // assignment to global memory
 
 	stk = VAL + stk; // assignment to register (indirect)
+
+	f(VAL); // assignment to argument
 }
