5eac5ef47fa941acc0e5f8a2447ab5a0b3c52f17
Rob Pilling <robpilling@gmail.com>
2019-08-10 11:03:04 +0100

    Fix common-init test

--- test/cases/initialisation/common_init.c
+++ test/cases/initialisation/common_init.c
@@ -1,4 +1,4 @@
-// RUN: %ucc -fno-leading-underscore -mno-align-is-p2 -S -o- %s | grep '\(common\|space\)' | grep -vF '\.(globl|type|size)' | %stdoutcheck %s
+// RUN: %ucc -fno-leading-underscore -mno-align-is-p2 -S -o- %s | grep 'common\|space' | grep -vE '\.(globl|type|size)' | %stdoutcheck %s
 // STDOUT: nocommon:
 // STDOUT-NEXT: .space 4 # object space
 // STDOUT-NEXT: .comm common,4,4
