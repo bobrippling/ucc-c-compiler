8ff6fa2caa8ffe7e7b2adca328c63d61e7330462
Rob Pilling <robpilling@gmail.com>
2019-11-23 18:23:46 +0000

    Merge typedef-array tests

--- test/cases/initialisation/array/via_typedef_incomplete.c
+++
@@ -1,5 +0,0 @@
-// RUN: %layout_check %s
-
-typedef int A[];
-A ent1 = { 1, 2 },
-  ent2 = { 3, 4, 5 };
--- test/cases/typedef_typeof/tdef_ar.c
+++ test/cases/typedef_typeof/tdef_ar.c
@@ -1,15 +1,48 @@
-// RUN: %ucc %s; [ $? -ne 0 ]
-// RUN: %ucc -DINIT %s; [ $? -eq 0 ]
+// RUN: %check -e --only %s -DERROR -std=c2x
+// RUN: %ucc -fsyntax-only %s
+//
+// RUN: %layout_check %s
 
-main()
+typedef int Flex[];
+Flex;
+
+// --------------------------
+
+
+void error()
 {
 	typedef int A[];
 
-	A x
-#ifdef INIT
+	A x // CHECK: error: "x" has incomplete type 'A {aka 'int[]'}'
+#ifndef ERROR
 		= { 1 }
 #endif
 	;
+}
 
-	return 3;
+// --------------------------
+
+Flex ent1 = { 1, 2 },
+		ent2 = { 3, 4, 5 };
+
+// --------------------------
+
+struct A {
+	short n;
+	// pad of 2
+	Flex ar;
+};
+
+struct B {
+	int n;
+	// no pad
+	Flex ar;
+};
+
+void ok()
+{
+	static struct A x = { 5, { 1, 2, 3 } }; // CHECK: warning: initialisation of flexible array (GNU)
+	_Static_assert(sizeof x == sizeof(int));
+
+	_Static_assert(sizeof(struct A) == sizeof(int));
 }
