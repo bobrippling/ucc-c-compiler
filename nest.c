fbe6a8e67bb346338d398ba319277c08d7a0698c
Rob Pilling <robpilling@gmail.com>
2020-01-15 18:27:21 +0000

    Repro

---
+++ nest.c
@@ -0,0 +1,23 @@
+int x;
+
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+int j;
+int k;
+int l;
+int m;
+int n;
+int o;
+int p;
+int q;
+
+hi() {
+	x = (a + (b + (c + (d + (e + (f + (g + (h + (i + (j + (k + (l + (m + (n + (o + (p + (q)))))))))))))))));
+}
