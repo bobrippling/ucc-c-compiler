5c7837d372d9b86f561cfbc3bd34b0f6ae636acd
Rob Pilling <robpilling@gmail.com>
2020-02-26 19:54:38 +0000

    t_asm/eg/nostd.c

---
+++ t_asm/eg/nostd.c
@@ -0,0 +1,29 @@
+//#include <unistd.h>
+//#include <sys/syscall.h>
+
+void start() //asm("start")
+{
+	//register int    syscall_no  asm("rax") = 1;
+	//register int    arg1        asm("rdi") = 1;
+	//register char*  arg2        asm("rsi") = "hello, world!\n";
+	//register int    arg3        asm("rdx") = 14;
+
+	asm(
+			"syscall"
+			:
+			: "a"(33554436)
+			, "D"(1)
+			, "S"("hello\n")
+			, "d"(6)
+	);
+
+	asm(
+			//"movl $33554433,%eax;"
+			//"xorl %ebx,%ebx;"
+			"syscall"
+			:
+			: "a"(33554433)
+			, "D"(61)
+		 );
+}
+
