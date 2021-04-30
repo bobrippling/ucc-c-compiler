1c3f8674598b7e0324065e1e8a0c1cad71db3a78
Rob Pilling <robpilling@gmail.com>
2021-04-30 18:33:05 +0100

    More examples of __attribute((naked))

--- naked.c
+++ naked.c
@@ -8,6 +8,27 @@ int f(int a, int b)
 	//return 3; - non-asm statement not allowed
 }
 
+__attribute__((naked))
+int f(int x, int y)
+{
+    __asm__ (
+        "lea (%rdi, %rsi, 1), %rax\n"
+        "ret\n"
+    );
+}
+
+__attribute__((naked))
+int g(int x, int y)
+{
+    __asm__ (
+        "push %rbp"
+        "mov %rsp, %rbp"
+        "lea (%rdi, %rsi, 1), %rax\n"
+        "pop %rbp"
+        "ret\n"
+    );
+}
+
 int main()
 {
 	int x = f(1, 2);
