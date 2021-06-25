681bebfab31bf0e81ce95459f9f777541a32a60d
Rob Pilling <robpilling@gmail.com>
2021-06-25 18:05:11 +0100

    Disable randstack if __attribute__((weak)) isn't supported
    And migrate a test to use %check

--- test/cases/ocheck-init.c
+++ test/cases/ocheck-init.c
@@ -1,7 +1,11 @@
 // RUN: true
 
 do{
+#if !defined(__DARWIN__)
 void randomise_stack(void) __attribute__((weak));
 if(randomise_stack)
 	randomise_stack();
+#else
+// weak support not present here
+#endif
 }while(0);
