d87e5ae722dc7a0461cc8cd42583f1c608decd0b
Rob Pilling <robpilling@gmail.com>
2021-06-27 23:24:00 +0100

    e

--- __asm/errors.c
+++
@@ -1,8 +0,0 @@
-main()
-{
-	int i, j;
-
-	__asm("%2" : "=r"(i) : "0"(0)); // CHECK: invalid register index %2
-	__asm("%2" : "=r"(i), "=r"(j)); // CHECK: invalid register index %2
-	__asm("%2" : : "r"(0), "r"(1)); // CHECK: invalid register index %2
-}
