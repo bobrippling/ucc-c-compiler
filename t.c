dfc5978e423e1889a014879523e3fa0f95e6b891
Rob Pilling <robpilling@gmail.com>
2021-09-13 22:36:42 +0100

    t.c

--- t.c
+++ t.c
@@ -1,6 +1,39 @@
+#if 0
+#include <pthread.h>
+
 static __thread int i = 3;
 
-int p()
+int printf();
+
+void *t(void *c)
 {
-	return i;
+	printf("thread %d, i = %d, &i = %p\n", *(int *)c, i, &i);
+	return 0;
 }
+
+int main()
+{
+	struct {
+		pthread_t p;
+		int i;
+	} ts[4];
+
+	for(int i = 0; i < sizeof(ts)/sizeof(*ts); i++){
+		ts[i].i = i;
+		pthread_create(&ts[i].p, 0, t, &ts[i].i);
+	}
+
+	for(int i = 0; i < sizeof(ts)/sizeof(*ts); i++){
+		pthread_join(ts[i].p, 0);
+	}
+}
+#else
+static __thread int i = 3;
+static __thread struct { int x, y; } s = { 3, 5 };
+static __thread double d = 9.4;
+
+int *p()
+{
+	return &i;
+}
+#endif
