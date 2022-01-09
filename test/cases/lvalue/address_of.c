c45fbd3400bf6a9bcb9bc98a5262e3d15958520d
Rob Pilling <robpilling@gmail.com>
2022-01-09 22:46:40 +0000

    Add addr-of incomplete-type test (&*...)

--- test/cases/lvalue/address_of.c
+++ test/cases/lvalue/address_of.c
@@ -1,4 +1,4 @@
-// RUN: %check %s
+// RUN: %check --only %s
 
 struct A
 {
@@ -8,11 +8,33 @@ struct A
 
 void g(void);
 
-void f(struct A *p)
+void tautology(struct A *p)
 {
 	if(&p->buf) // CHECK: warning: address of lvalue (char[3]) is always true
 		g();
 
 	if(&*p) // CHECK: warning: address of lvalue (struct A) is always true
 		g();
+}
+
+// -------------------------------------------
+
+void take_any(void *);
+
+void func_and_array_addressable()
+{
+	int f();
+	int x[2];
+
+	take_any(&f); // CHECK: /warning: implicit cast/
+	take_any(&x);
+}
+
+// -------------------------------------------
+
+struct Incomplete;
+
+void *f(struct Incomplete *p)
+{
+	return &*p;
 }
