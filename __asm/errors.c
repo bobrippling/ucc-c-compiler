9bf5fae98d2a07ccbc45e9b261950863cb1af6b1
Rob Pilling <robpilling@gmail.com>
2022-11-29 23:11:55 +0000

    rm __asm/<stuff> ?

---
+++ __asm/errors.c
@@ -0,0 +1,8 @@
+main()
+{
+	int i, j;
+
+	__asm("%2" : "=r"(i) : "0"(0)); // CHECK: invalid register index %2
+	__asm("%2" : "=r"(i), "=r"(j)); // CHECK: invalid register index %2
+	__asm("%2" : : "r"(0), "r"(1)); // CHECK: invalid register index %2
+}
