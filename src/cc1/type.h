f4315c23054f3e184fd5a1cfc042a85258599b4c
Rob Pilling <robpilling@gmail.com>
2024-02-16 18:15:57 +0000

    Add type_min()

--- src/cc1/type.h
+++ src/cc1/type.h
@@ -141,6 +141,7 @@ enum type_str_type
 enum type_str_type type_str_type(type *);
 
 integral_t type_max(type *r, where *from);
+sintegral_t type_min(type *r, where *from);
 
 where *type_loc(type *);
 where *type_has_loc(type *);
